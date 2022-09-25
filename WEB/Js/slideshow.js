4341bc237f221fcc18b27173a22aaa8a1c286df4
Messi002 <rostandmessi2@gmail.com>
2022-09-25 07:54:27 +0100

    cssjs

--- WEB/Js/slideshow.js
+++ WEB/Js/slideshow.js
@@ -1,35 +1,9 @@
 let slides = document.querySelectorAll('.myslides');
-let pbtn = document.querySelector('.prev');
-let slideIndex = 1;
+let pbtn = document.querySelector('#prev');
+let current = 0;
 
 
-//next and previous button
-// const plusSlides = (n) => {
-//     showSlides(slideIndex += n);
-// };
-pbtn.addEventListener('click', (e)=>{
-    stop.
-console.log('fired');
-});
-
-// Thumbnail image controls
-const currentSlide = (n) => {
-    showSlides(slideIndex = n);
-  };
-
-  
-const showSlides = (n) => {
-if (n > slides.length) {
-    slideIndex = 1;
-}
-
-for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}
-slides[n].style.display = 'block';
-};
-
-showSlides(slideIndex);
+//clear all images
 
 
 // export { flowers };
