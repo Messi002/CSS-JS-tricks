782d9387747844165e3a3a48b44c6d27fb5a03fa
Messi002 <rostandmessi2@gmail.com>
2022-09-25 06:51:11 +0100

    cssjs

--- WEB/Js/slideshow.js
+++ WEB/Js/slideshow.js
@@ -2,6 +2,13 @@ let mySlides = document.querySelector('.myslides');
 let slideIndex = 1;
 showSlides(slideIndex);
 
+//next and previous button
+const plusSlides = (n) => {
+    showSlides(slideIndex += n);
+};
+
+const showSlides = () => {};
+
 
 
 // export { flowers };
