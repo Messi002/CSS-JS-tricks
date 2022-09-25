63ab2c29c5fe8df5763022c9260ca5cd71487c54
Messi002 <rostandmessi2@gmail.com>
2022-09-25 07:57:35 +0100

    cssjs

--- WEB/Js/slideshow.js
+++ WEB/Js/slideshow.js
@@ -4,6 +4,15 @@ let current = 0;
 
 
 //clear all images
+const reset = () => {
+    for (let i = 0; i < array.length; i++) {
+            slides[i].style.display ='none';        
+    }
+}
 
+//initial slide
+const startSlide = () =>  {
+    
+}
 
 // export { flowers };
