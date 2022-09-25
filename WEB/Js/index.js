4a972d04e7c31d4f9e86ddc5b753d11a4d6a9891
Messi002 <rostandmessi2@gmail.com>
2022-09-25 04:42:56 +0100

    cssjs

---
+++ WEB/Js/index.js
@@ -0,0 +1,4 @@
+import sum, {num} from './slideshow.js';
+
+console.log(sum(5, 5)); 
+console.log(num);
---
+++ WEB/Js/slideshow.js
@@ -0,0 +1,14 @@
+// 👇️ default export
+// export default function sum(a, b) {
+//     return a + b;
+//   }
+  
+  // 👇️ named export
+//   export const num = 100;
+
+const greet = 'greetings'
+
+module.exports = {
+    greet,
+    message,
+  };
