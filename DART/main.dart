2c3443f44dfbd085d047c95315d624a033edec49
Messi002 <rostandmessi2@gmail.com>
2022-09-27 16:44:39 +0100

    dart

--- DART/main.dart
+++ DART/main.dart
@@ -1,45 +1,47 @@
-import 'dart:io';
+// import 'dart:io';
 
-enum Weather {
-  sunny,
-  cloudy,
-  rainy,
-  windy,
-}
+// enum Weather {
+//   sunny,
+//   cloudy,
+//   rainy,
+//   windy,
+// }
 
-void main(List<String> args) {
-  print('Weather Conditions');
-  print(Weather.cloudy.index);
+// void main(List<String> args) {
+//   print('Weather Conditions');
+//   print(Weather.cloudy.index);
 
-  const weather = Weather.rainy;
+//   const weather = Weather.rainy;
 
-  switch (weather) {
-    case Weather.cloudy:
-      print('It is cloudy today');
-      break;
-    case Weather.rainy:
-      print('It is rainy today');
-      break;
-    case Weather.sunny:
-      print('It is sunny today');
-      break;
-    case Weather.windy:
-      print('Leave out the umbrella');
-      break;
-    default:
-      print('Strange, really strange');
-  }
+//   switch (weather) {
+//     case Weather.cloudy:
+//       print('It is cloudy today');
+//       break;
+//     case Weather.rainy:
+//       print('It is rainy today');
+//       break;
+//     case Weather.sunny:
+//       print('It is sunny today');
+//       break;
+//     case Weather.windy:
+//       print('Leave out the umbrella');
+//       break;
+//     default:
+//       print('Strange, really strange');
+//   }
 
-  User user = User();
-  print(user);
-}
+//   User user = User();
+//   print(user);
+// }
 
-class User {
-  final name = 'Ram';
-  final id = 0;
+// class User {
+//   final name = 'Ram';
+//   final id = 0;
 
-  @override
-  String toString() {
-    return '{"name": "$name", "id": $id}';
-  }
-}
+//   @override
+//   String toString() {
+//     return '{"name": "$name", "id": $id}';
+//   }
+// }
+
+
