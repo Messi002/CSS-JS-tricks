2e1b133944c60a026a19d74da947b78f32054a93
Messi002 <rostandmessi2@gmail.com>
2022-09-27 03:41:52 +0100

    dart

--- DART/main.dart
+++ DART/main.dart
@@ -1,2 +1,30 @@
 import 'dart:io';
 
+enum Weather {
+  sunny,
+  cloudy,
+  rainy,
+  windy,
+}
+
+void main(List<String> args) {
+  print('Weather Conditions');
+
+  const weather = Weather.rainy;
+
+  switch (weather) {
+    case value:
+      
+      break;
+    case value:
+      
+      break;
+      case value:
+      
+      break;
+      case value:
+      
+      break;
+    default:
+  }
+}
