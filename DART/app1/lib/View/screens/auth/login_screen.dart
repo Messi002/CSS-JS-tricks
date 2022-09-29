48a83f2d500bff38b1b00dc09b963ffa1f041398
Messi002 <rostandmessi2@gmail.com>
2022-09-29 22:07:15 +0100

    dart

--- DART/app1/lib/View/screens/auth/login_screen.dart
+++ DART/app1/lib/View/screens/auth/login_screen.dart
@@ -5,14 +5,13 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  
+
   LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-       
         alignment: Alignment.center,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -38,41 +37,51 @@ class LoginScreen extends StatelessWidget {
               child: TextInputField(
                   controller: _emailController,
                   labelText: "Email",
-                  
                   icon: Icons.email_rounded),
             ),
-             SizedBox(height: 25),
-             Container(
+            SizedBox(height: 25),
+            Container(
               width: MediaQuery.of(context).size.width,
               margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               child: TextInputField(
                   controller: _passwordController,
                   labelText: "Password",
-                  
                   icon: Icons.lock_rounded),
             ),
-             SizedBox(height: 30),
-             Container(
-              width: MediaQuery.of(context).size.width-40,
+            SizedBox(height: 30),
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
               height: 50,
               decoration: BoxDecoration(
                 color: kButtonColor,
                 borderRadius: const BorderRadius.all(Radius.circular(5)),
               ),
               child: InkWell(
-                onTap: (){}, //TODO: HERE
-                child: const Center(child: Text('Login', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),))),
-             ),
-             const SizedBox(height: 15),
-             Row(
+                  onTap: () {
+                    debugPrint('welceome');
+                  }, //TODO: HERE
+                  child: const Center(
+                      child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                  ))),
+            ),
+            const SizedBox(height: 15),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text("Don't have an account?", style: TextStyle(fontSize: 20),),
-                 InkWell(
-                  onTap: (){}, //TODO: HERE
-                  child: Text("Register", style: TextStyle(fontSize: 20, color: kButtonColor),))
+                const Text(
+                  "Don't have an account?",
+                  style: TextStyle(fontSize: 20),
+                ),
+                InkWell(
+                    onTap: () {}, //TODO: HERE
+                    child: Text(
+                      "Register",
+                      style: TextStyle(fontSize: 20, color: kButtonColor),
+                    ))
               ],
-             )
+            )
           ],
         ),
       ),
