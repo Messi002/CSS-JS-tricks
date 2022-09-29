db4a3bca9bdd2b74ef887b3a6cacf7c39240e6f3
Messi002 <rostandmessi2@gmail.com>
2022-09-30 00:01:15 +0100

    dart

--- DART/app1/lib/Controller/auth_controller.dart
+++ DART/app1/lib/Controller/auth_controller.dart
@@ -1,5 +1,16 @@
+import 'dart:io';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController{
-  
+
+  //registering the user
+  void registerUser(String username, String email, String password, File? image ) async{
+    try{
+
+    }
+    catch{
+      Get.snackbar(title, message)
+    }
+  }
+
 }
