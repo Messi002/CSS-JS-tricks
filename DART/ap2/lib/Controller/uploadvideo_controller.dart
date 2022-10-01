73d6ff8539cde17eb770fa72cd597e2b96d00bcf
Messi002 <rostandmessi2@gmail.com>
2022-10-01 23:50:10 +0100

    dart

--- DART/ap2/lib/Controller/uploadvideo_controller.dart
+++ DART/ap2/lib/Controller/uploadvideo_controller.dart
@@ -1,15 +1,14 @@
-
-
+import 'package:ap2/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
-class UpLoadVideoController extends GetxController{
-  
+class UpLoadVideoController extends GetxController {
   //function to upload video...
-  uploadVideo(String songName, String caption, String videopath) async{
+  uploadVideo(String songName, String caption, String videopath) async {
     try {
-      
-    } catch (e) {
-      
-    }
+      String uid = firebaseAuth.currentUser!.uid;
+      DocumentSnapshot userDoc =
+          await firestore.collection('users').doc('uid').get();
+    } catch (e) {}
   }
 }
