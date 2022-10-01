47ecf5494e68b28dfd0add8eef3694c2dd2282ef
Messi002 <rostandmessi2@gmail.com>
2022-10-02 00:00:35 +0100

    dart

--- DART/ap2/lib/Controller/uploadvideo_controller.dart
+++ DART/ap2/lib/Controller/uploadvideo_controller.dart
@@ -1,12 +1,14 @@
 import 'package:ap2/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 
 class UpLoadVideoController extends GetxController {
 
 
   _uploadVideoToStorage(String id, String videopath){
-    
+    Reference ref = firebaseStorage.ref().child('videos').child(id);
+
   }
 
   //function to upload video...
