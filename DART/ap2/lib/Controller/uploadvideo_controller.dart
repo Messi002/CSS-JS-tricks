7f85769ecc37669e98314406e17359b60933d02d
Messi002 <rostandmessi2@gmail.com>
2022-10-01 23:56:17 +0100

    dart

--- DART/ap2/lib/Controller/uploadvideo_controller.dart
+++ DART/ap2/lib/Controller/uploadvideo_controller.dart
@@ -3,12 +3,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class UpLoadVideoController extends GetxController {
+
+
+  _uploadVideoToStorage(String id, String videopath){
+    
+  }
+
   //function to upload video...
   uploadVideo(String songName, String caption, String videopath) async {
     try {
       String uid = firebaseAuth.currentUser!.uid;
       DocumentSnapshot userDoc =
-          await firestore.collection('users').doc('uid').get();
+          await firestore.collection('users').doc(uid).get();
+      //get id
+      var allDocs = await firestore.collection('videos').get();
+      int len = allDocs.docs.length;
+      _uploadVideoToStorage("Video $len", videopath);
     } catch (e) {}
   }
 }
