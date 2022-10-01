1d05ad5c6314b318b494e7daf303b4512f747063
Messi002 <rostandmessi2@gmail.com>
2022-10-01 23:46:27 +0100

    dart

--- DART/ap2/lib/Controller/uploadvideo_controller.dart
+++ DART/ap2/lib/Controller/uploadvideo_controller.dart
@@ -4,4 +4,12 @@ import 'package:get/get.dart';
 
 class UpLoadVideoController extends GetxController{
   
+  //function to upload video...
+  uploadVideo(String songName, String caption, String videopath) async{
+    try {
+      
+    } catch (e) {
+      
+    }
+  }
 }
