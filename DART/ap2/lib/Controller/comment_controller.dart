b8b1079889e62d3d47b8b7540b7ca4e189d32f3d
Messi002 <rostandmessi2@gmail.com>
2022-10-02 09:46:07 +0100

    dart

--- DART/ap2/lib/Controller/comment_controller.dart
+++ DART/ap2/lib/Controller/comment_controller.dart
@@ -1,9 +1,33 @@
-
-
+import 'package:ap2/Models/comment_model.dart';
+import 'package:ap2/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
-class CommentController extends GetxController{
+class CommentController extends GetxController {
+  final Rx<List<CommentModel>> _comments = Rx<List<CommentModel>>([]);
+  List<CommentModel> get comments => _comments.value;
 
+  String _postId = "";
+  updatePostId(String id) {
+    _postId = id;
+    getComments();
+  }
 
+  getComments() async {}
 
+  postComment(String commentText) async {
+    if (commentText.isNotEmpty) {
+      DocumentSnapshot userDoc = await firestore
+          .collection('users')
+          .doc(authController.user!.uid)
+          .get();
+      var allDocs = await firestore
+          .collection('videos')
+          .doc(_postId)
+          .collection('comments')
+          .get();
+      int len = allDocs.docs.length;
+      CommentModel comment = CommentModel(datePublished: datePublished, username: username, comment: comment, likes: likes, photoUrl: photoUrl, uid: uid, id: id)
+    }
+  }
 }
