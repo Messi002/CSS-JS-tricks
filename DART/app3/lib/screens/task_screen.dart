714f59a8049564913603ae28426660978a32412a
Messi002 <rostandmessi2@gmail.com>
2022-10-04 03:28:52 +0100

    dart

--- DART/app3/lib/screens/task_screen.dart
+++ DART/app3/lib/screens/task_screen.dart
@@ -7,6 +7,28 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class TasksScreen extends StatelessWidget {
   TasksScreen({Key? key}) : super(key: key);
 
+  final TextEditingController _titleController = TextEditingController();
+
+  void _addTask(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: Column(
+                  children: [
+                   const Text('Add Task...',style: TextStyle(fontSize: 14),),
+                   const SizedBox(height: 16),
+                    TextField(
+                      controller: _titleController,
+                    ),
+                  ],
+                ),
+              ),
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TasksBloc, TasksState>(
@@ -36,7 +58,7 @@ class TasksScreen extends StatelessWidget {
             ],
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: () {},
+            onPressed: ()=> _addTask(context),
             tooltip: 'Add Task',
             child: const Icon(Icons.add),
           ),
