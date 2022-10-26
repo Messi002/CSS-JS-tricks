60465640775a0f2d34010525fd19e5b5d5aeb636
Messi002 <rostandmessi2@gmail.com>
2022-10-26 10:52:53 +0100

    dart

--- DART/expt.dart
+++ DART/expt.dart
@@ -1,43 +1,56 @@
-void main(){
-
-
+void main() {
   List productType = [
     {'product_type': 'Ohrring', 'colour': 'silberfarben'},
-     {'product_type': 'Anhänger', 'colour': 'Blau'},
+    {'product_type': 'Anhänger', 'colour': 'Blau'},
   ];
-List userPref = ['silberfarben','Blau'];
-  
+  List? userPref = ['Anhänger', 'Rosatd'];
+
   List colorType = [
     {'product_type': 'Anhänger', 'colour': 'silberfarben'},
     {'product_type': 'Rosa', 'colour': 'silberfarben'},
     {'product_type': 'Rosatd', 'colour': 'Blau'},
     {'product_type': 'Ohrring', 'colour': 'goldfarben'}
   ];
-  
-  List filtered = colorType.where((item)=> item == item).toList();
-  
-var gottenProducts = colorType.where((element) {
-  return element['colour'] == userPref[0] || element['colour'] == userPref[1];
-});
-   
+
+  List filtered = colorType.where((item) => item == item).toList();
+
+  int pdtLen = userPref.length;
+
+  switch (pdtLen) {
+    case 1:
+      print('number $pdtLen here');
+      break;
+    case 2:
+      print('number $pdtLen here');
+
+      break;
+    case 3:
+      print('number $pdtLen here');
+
+      break;
+    case 4:
+      print('number $pdtLen here');
+
+      break;
+    case 5:
+      print('number $pdtLen here');
+
+      break;
+    case 6:
+      print('number $pdtLen here');
+
+      break;
+    default:
+      print('null list');
+  }
+
   // for (int i = 0; i < colorType.length; i++){
   //   print('$i');
   //   if(colorType[i]['colour'] == userPref[i] || colorType[i]['colour'] == userPref[i+1]){
-      
+
   //     gottenProducts.add(colorType[i]);
   //   }
   // }
 
-
-
-
-
-
-
-
-
-
-  print("${gottenProducts.toList()}");
-  
- 
+  // print("${gottenProducts.toList()}");
 }
