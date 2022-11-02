68a69f991dac5ad8f862e26ee1c6e3ea8506fd58
Messi002 <rostandmessi2@gmail.com>
2022-11-02 11:18:32 +0100

    bloc_training

--- DART/app4/lib/timer_event.dart
+++ DART/app4/lib/timer_event.dart
@@ -12,3 +12,25 @@ class TimerStarted extends TimerEvent {
   const TimerStarted({required this.duration});
   final int duration;
 }
+
+class TimerPaused extends TimerEvent {
+  const TimerPaused();
+}
+
+class TimerResumed extends TimerEvent {
+  const TimerResumed();
+}
+
+
+class TimerReset extends TimerEvent {
+  const TimerReset();
+}
+
+
+class TimerTicked extends Equatable {
+  const TimerTicked({required this.duration});
+  final int duration;
+
+  @override
+  List<Object> get props => [duration];
+}
