cf91ac4ce3bf5ff5af9ed10c25e2a3a9de052d24
Messi002 <rostandmessi2@gmail.com>
2022-11-03 03:45:26 +0100

    dart

---
+++ DART/app4/TimerBloc/timer.dart
@@ -0,0 +1,80 @@
+// import 'package:app4/actions.dart';
+// import 'package:app4/ticker.dart';
+// import 'package:app4/timer_bloc.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+
+
+// class TimerPage extends StatelessWidget {
+//   const TimerPage({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocProvider(
+//       create: (_) => TimerBloc(ticker: Ticker()),
+//       child: const TimerView(),
+//     );
+//   }
+// }
+
+// class TimerView extends StatelessWidget {
+//   const TimerView({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Flutter Timer')),
+//       body: Stack(
+//         children: [
+//           const Background(),
+//           Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: const <Widget>[
+//               Padding(
+//                 padding: EdgeInsets.symmetric(vertical: 100.0),
+//                 child: Center(child: TimerText()),
+//               ),
+//               ActionCenter(),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class TimerText extends StatelessWidget {
+//   const TimerText({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+//     final minutesStr =
+//         ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+//     final secondsStr = (duration % 60).floor().toString().padLeft(2, '0');
+//     return Text(
+//       '$minutesStr:$secondsStr',
+//       style: Theme.of(context).textTheme.headline1,
+//     );
+//   }
+// }
+
+
+// class Background extends StatelessWidget {
+//   const Background({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           begin: Alignment.topCenter,
+//           end: Alignment.bottomCenter,
+//           colors: [
+//             Colors.blue.shade50,
+//             Colors.blue.shade500,
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
