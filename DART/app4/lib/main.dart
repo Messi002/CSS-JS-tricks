a44f2e28e5efb0127732e0804f1a2e5917667db6
Messi002 <rostandmessi2@gmail.com>
2022-11-03 04:06:23 +0100

    dart

---
+++ DART/app4/lib/main.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+       theme: ThemeData(
+        primaryColor: const Color.fromRGBO(109, 234, 255, 1),
+        colorScheme: const ColorScheme.light(
+          secondary: Color.fromRGBO(72, 74, 126, 1),
+        ),
+      ),
+   home: Container(),
+    );
+  }
+}
