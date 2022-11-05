a7ffa74ffcc0e3611044059e4e100cf2321dddd4
Messi002 <rostandmessi2@gmail.com>
2022-11-05 04:33:48 +0100

    bloc_training

--- DART/app4/lib/posts/view/posts_list.dart
+++ DART/app4/lib/posts/view/posts_list.dart
@@ -1,7 +1,11 @@
+import 'package:app4/posts/bloc/post_bloc.dart';
+import 'package:app4/posts/widgets/bottom_loader.dart';
+import 'package:app4/posts/widgets/post_list_item.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PostsList extends StatefulWidget {
   const PostsList({super.key});
@@ -11,11 +15,41 @@ class PostsList extends StatefulWidget {
 }
 
 class _PostsListState extends State<PostsList> {
+  ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
   @override
   Widget build(BuildContext context) {
+    return BlocBuilder<PostBloc, PostState>(
+      builder: (context, state) {
+        switch (state.status) {
+          case PostStatus.failure:
+            return const Center(
+              child: Text('failed to fetch posts'),
+            );
 
-
-
-    return Container();
+          case PostStatus.success:
+            if (state.posts.isEmpty) {
+              return const Center(child: Text('no posts'));
+            }
+            return ListView.builder(
+              controller: _scrollController,
+              itemCount: state.hasReachedMax ? state.posts.length : state.posts.length + 1,
+              itemBuilder: (context, index) {
+              return index >= state.posts.length
+                  ? BottomLoader()
+                  : PostListItem(post: state.posts[index]);
+                
+            });
+          case PostStatus.initial:
+            return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
   }
 }
