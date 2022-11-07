e1e0d610d218e5ee6d65315cd3b9b9cd40f356a6
Messi002 <rostandmessi2@gmail.com>
2022-11-07 21:26:10 +0100

    bloc_training

--- DART/app4/lib/bloc/authentication_bloc.dart
+++ DART/app4/lib/bloc/authentication_bloc.dart
@@ -1,4 +1,9 @@
+import 'dart:html';
+
+import 'package:app4/authentication_repository.dart';
+import 'package:app4/user.dart';
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'authentication_event.dart';
--- DART/app4/lib/bloc/authentication_state.dart
+++ DART/app4/lib/bloc/authentication_state.dart
@@ -1,6 +1,24 @@
 part of 'authentication_bloc.dart';
 
-@immutable
-abstract class AuthenticationState {}
+class AuthenticationState extends Equatable {
+  const AuthenticationState._({
+    this.status = AuthenticationStatus.unknown,
+    this.user = UserModel.empty,
+  });
 
-class AuthenticationInitial extends AuthenticationState {}
+  final UserModel user;
+  final AuthenticationStatus status;
+
+  const AuthenticationState.unknown() : this._();
+
+  const AuthenticationState.authenticated(UserModel user)
+      : this._(status: AuthenticationStatus.authenticated, user: user);
+
+  const AuthenticationState.unauthenticated()
+      : this._(status: AuthenticationStatus.unauthenticated);
+
+  @override
+  List<Object> get props => [status, user];
+}
+
+// class AuthenticationInitial extends AuthenticationState {}
