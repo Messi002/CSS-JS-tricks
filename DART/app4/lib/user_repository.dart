af000da762fbf0aeed496896ab7199c0a212c3d3
Messi002 <rostandmessi2@gmail.com>
2022-11-07 20:50:53 +0100

    bloc_training

---
+++ DART/app4/lib/user_repository.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'package:app4/user.dart';
+import 'package:uuid/uuid.dart';
+
+class UserRepository {
+  UserModel? _user;
+
+  Future<UserModel?> getUser() async {
+    
+  }
+}
