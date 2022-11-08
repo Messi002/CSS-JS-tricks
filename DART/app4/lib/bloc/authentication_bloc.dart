f2487d9b05c9e7a781bb4fb0d96486f586d0a06d
Messi002 <rostandmessi2@gmail.com>
2022-11-08 21:11:14 +0100

    bloc_training

--- DART/app4/lib/bloc/authentication_bloc.dart
+++ DART/app4/lib/bloc/authentication_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 
 import 'package:app4/authentication_repository.dart';
 import 'package:app4/user.dart';
+import 'package:app4/user_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
@@ -10,9 +11,17 @@ part 'authentication_event.dart';
 part 'authentication_state.dart';
 
 class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
-  AuthenticationBloc() : super(AuthenticationInitial()) {
+  AuthenticationBloc({
+    required AuthenticationRepository authenticationRepository,
+    required UserRepository userRepository
+  }) : super(const AuthenticationInitial()) {
     on<AuthenticationEvent>((event, emit) {
       // TODO: implement event handler
     });
   }
+
+    final AuthenticationRepository _authenticationRepository;
+  final UserRepository _userRepository;
+  late StreamSubscription<AuthenticationStatus>
+      _authenticationStatusSubscription;
 }
