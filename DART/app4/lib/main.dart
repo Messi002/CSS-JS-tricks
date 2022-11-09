337fff5a3d7851271a7943835433df3ae30fa271
Messi002 <rostandmessi2@gmail.com>
2022-11-09 22:32:41 +0100

    bloc_training

--- DART/app4/lib/main.dart
+++ DART/app4/lib/main.dart
@@ -1,16 +1,48 @@
+import 'package:app4/authentication_repository.dart';
+import 'package:app4/bloc/authentication_bloc.dart';
+import 'package:app4/user_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp(
-    authenticationRepository : AuthenticationRepository(),
+  runApp(MyApp(
+    authenticationRepository: AuthenticationRepository(),
     userRepository: UserRepository(),
   ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+    required this.authenticationRepository,
+    required this.userRepository,
+  });
 
+  final AuthenticationRepository authenticationRepository;
+  final UserRepository userRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+            authenticationRepository: authenticationRepository,
+            userRepository: userRepository),
+        child: const AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  const AppView({super.key});
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,5 +56,4 @@ class MyApp extends StatelessWidget {
       home: Container(),
     );
   }
-// }
 }
