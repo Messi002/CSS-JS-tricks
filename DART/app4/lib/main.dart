d5ea29f8bd99b574838d82931fed7096373e2a17
Messi002 <rostandmessi2@gmail.com>
2022-11-09 22:46:33 +0100

    bloc_training

--- DART/app4/lib/main.dart
+++ DART/app4/lib/main.dart
@@ -50,14 +50,34 @@ class _AppViewState extends State<AppView> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: const Color.fromRGBO(109, 234, 255, 1),
-        colorScheme: const ColorScheme.light(
-          secondary: Color.fromRGBO(72, 74, 126, 1),
-        ),
-      ),
-      home: Container(),
+      navigatorKey: _navigatorKey,
+      builder: (context, child) {
+        return BlocListener<AuthenticationBloc, AuthenticationState>(listener: (context, state){
+          switch (state.status) {
+            case value:
+              
+              break;
+             case value:
+              
+              break;
+             case value:
+              
+              break;
+            default:
+          }
+        });
+      },
     );
   }
 }
+
+
+
+//  title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primaryColor: const Color.fromRGBO(109, 234, 255, 1),
+//         colorScheme: const ColorScheme.light(
+//           secondary: Color.fromRGBO(72, 74, 126, 1),
+//         ),
+//       ),
+//       home: Container(),
