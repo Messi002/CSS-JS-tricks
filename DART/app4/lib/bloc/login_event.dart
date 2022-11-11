9daa534e3c0d5f60edb674125e1f11f43137deaf
Messi002 <rostandmessi2@gmail.com>
2022-11-11 23:14:13 +0100

    bloc_training

--- DART/app4/lib/bloc/login_event.dart
+++ DART/app4/lib/bloc/login_event.dart
@@ -11,4 +11,11 @@ List<Object> get props => [];
 
 class LoginUsernameChanged extends LoginEvent{
   const LoginUsernameChanged(this.username);
+
+  final String username;
+
+  @override
+  List<Object> get props => [username];
 }
+
+cla
