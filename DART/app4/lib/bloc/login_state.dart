20daeaf31b589809b27385d5b7981789c1cccb86
Messi002 <rostandmessi2@gmail.com>
2022-11-11 23:22:39 +0100

    bloc_training

--- DART/app4/lib/bloc/login_state.dart
+++ DART/app4/lib/bloc/login_state.dart
@@ -1,6 +1,12 @@
 part of 'login_bloc.dart';
 
 @immutable
-abstract class LoginState {}
+class LoginState extends Equatable{
+  const LoginState({
+    this.status = FormzStatus.pure,
+    this.username = const Username.pure(),
+    this.password = const Password.pure()
+  });
+}
 
-class LoginInitial extends LoginState {}
+// class LoginInitial extends LoginState {}
