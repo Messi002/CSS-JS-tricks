7662870a1500d386c8ab6b71397c61df788a02a2
Messi002 <rostandmessi2@gmail.com>
2022-11-11 23:47:40 +0100

    bloc_training

--- DART/app4/lib/bloc/login_bloc.dart
+++ DART/app4/lib/bloc/login_bloc.dart
@@ -29,8 +29,22 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     final username = Username.dirty(event.username);
     emit(state.copyWith(
       username : username,
-      status : Formz.validate([state.password, username]);
+      status : Formz.validate([state.password, username])
     ));
 
   }
+
+
+  void _onPasswordChanged(
+    LoginPasswordChanged event, 
+    Emitter<LoginState> emit,
+  ){
+
+    final password = Password.dirty(event.password);
+    emit(state.copyWith(
+      password : password
+    ));
+  }
+
 }
+
