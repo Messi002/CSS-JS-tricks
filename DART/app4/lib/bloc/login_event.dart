b80b36bfed87bafdd6502d5075808a18a2c846ce
Messi002 <rostandmessi2@gmail.com>
2022-11-11 23:13:02 +0100

    bloc_training

---
+++ DART/app4/lib/bloc/login_event.dart
@@ -0,0 +1,14 @@
+part of 'login_bloc.dart';
+
+@immutable
+abstract class LoginEvent extends Equatable{
+const LoginEvent();
+
+@override
+List<Object> get props => [];
+
+}
+
+class LoginUsernameChanged extends LoginEvent{
+  const LoginUsernameChanged(this.username);
+}
