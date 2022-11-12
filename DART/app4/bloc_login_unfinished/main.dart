dc181d6322ae75087d83a02fee7dfc967581d10f
Messi002 <rostandmessi2@gmail.com>
2022-11-12 19:56:51 +0100

    bloc_training

---
+++ DART/app4/bloc_login_unfinished/main.dart
@@ -0,0 +1,87 @@
+// import 'package:app4/authentication_repository.dart';
+// import 'package:app4/bloc/authentication_bloc.dart';
+// import 'package:app4/user_repository.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+// void main() {
+//   runApp(MyApp(
+//     authenticationRepository: AuthenticationRepository(),
+//     userRepository: UserRepository(),
+//   ));
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({
+//     super.key,
+//     required this.authenticationRepository,
+//     required this.userRepository,
+//   });
+
+//   final AuthenticationRepository authenticationRepository;
+//   final UserRepository userRepository;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return RepositoryProvider.value(
+//       value: authenticationRepository,
+//       child: BlocProvider(
+//         create: (_) => AuthenticationBloc(
+//             authenticationRepository: authenticationRepository,
+//             userRepository: userRepository),
+//         child: const AppView(),
+//       ),
+//     );
+//   }
+// }
+
+// class AppView extends StatefulWidget {
+//   const AppView({super.key});
+
+//   @override
+//   State<AppView> createState() => _AppViewState();
+// }
+
+// class _AppViewState extends State<AppView> {
+//   final _navigatorKey = GlobalKey<NavigatorState>();
+
+//   NavigatorState get _navigator => _navigatorKey.currentState!;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       navigatorKey: _navigatorKey,
+//       builder: (context, child) {
+//         return BlocListener<AuthenticationBloc, AuthenticationState>(
+//             listener: (context, state) {
+//           switch (state.status) {
+//             case AuthenticationStatus.authenticated:
+//               _navigator.pushAndRemoveUntil<void>(
+//                   HomePage.route(), (route) => false);
+//               break;
+//             case AuthenticationStatus.unauthenticated:
+//               _navigator.pushAndRemoveUntil(Login.route(), (route) => false);
+//               break;
+//             case AuthenticationStatus.unknown:
+//               break;
+//           }
+//         },
+//         child: child,
+        
+//         );
+//       },
+//       onGenerateRoute: (_)=>SplashPage.route(),
+//     );
+//   }
+// }
+
+
+
+// //  title: 'Flutter Demo',
+// //       theme: ThemeData(
+// //         primaryColor: const Color.fromRGBO(109, 234, 255, 1),
+// //         colorScheme: const ColorScheme.light(
+// //           secondary: Color.fromRGBO(72, 74, 126, 1),
+// //         ),
+// //       ),
+// //       home: Container(),
