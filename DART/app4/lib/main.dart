861f259df992d37340342569ead7f1dc89dedc65
Messi002 <rostandmessi2@gmail.com>
2022-11-13 22:12:24 +0100

    bloc

--- DART/app4/lib/main.dart
+++ DART/app4/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
-import 'dart:math' as math show Random;
 
 void main() {
   runApp(const MyApp());
@@ -21,70 +20,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const names = ['Austin', 'Messi', 'Rostand','Gareth'];
 
-extension RandomElement<T> on Iterable<T> {
-  T getRandomElement() => elementAt(math.Random().nextInt(length));
+@immutable abstract class LoadAction{
+  
 }
 
-class NamesCubit extends Cubit<String?> {
-  NamesCubit() : super(null);
-
-  void pickRandomNames() => emit(names.getRandomElement());
-}
-
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late final NamesCubit cubit;
-
-  @override
-  void initState() {
-    super.initState();
-    cubit = NamesCubit();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    cubit.close();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('AppBar'),
-      ),
-      body: StreamBuilder<String?>(
-        stream: cubit.stream,
-        builder: (context, snapshot) {
-          final button = TextButton(
-              onPressed: () => cubit.pickRandomNames(),
-              child: const Text('pick a random name'));
-
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              return button;
-            case ConnectionState.waiting:
-              return button;
-            case ConnectionState.active:
-              return Column(
-                children: <Widget>[
-                  Text(snapshot.data ?? ' '),
-                  button
-                ],
-              );
-            case ConnectionState.done:
-              return const SizedBox();
-          }
-        },
-      ),
-    );
+    return Container();
   }
 }
