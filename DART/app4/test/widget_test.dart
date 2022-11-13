9bf56e3bc9f4749f0d07076c01cfa320733bcf30
Messi002 <rostandmessi2@gmail.com>
2022-11-13 22:02:28 +0100

    bloc

--- DART/app4/test/widget_test.dart
+++ DART/app4/test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:app4/main.dart';
+import 'package:app4/nothing_here.txt';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
