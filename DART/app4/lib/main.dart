b29d94775a63b9b787a993890e3e7b6caaa65b71
Messi002 <rostandmessi2@gmail.com>
2022-11-13 22:45:37 +0100

    bloc

--- DART/app4/lib/main.dart
+++ DART/app4/lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
@@ -26,6 +29,12 @@ abstract class LoadAction {
   const LoadAction();
 }
 
+@immutable
+abstract class LoadPersonAction implements LoadAction {
+  final PersonUrl url;
+  const LoadPersonAction({required this.url}) : super();
+}
+
 enum PersonUrl { person1, person2 }
 
 extension UrlString on PersonUrl {
@@ -39,13 +48,50 @@ extension UrlString on PersonUrl {
   }
 }
 
+@immutable
+class Person {
+  final String name;
+  final int age;
+  const Person({
+    required this.name,
+    required this.age,
+  });
+
+  Person copyWith({
+    String? name,
+    int? age,
+  }) {
+    return Person(
+      name: name ?? this.name,
+      age: age ?? this.age,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'name': name,
+      'age': age,
+    };
+  }
+
+  factory Person.fromJson(Map<String, dynamic> json) {
+    return Person(
+      name: json['name'] as String,
+      age: json['age'] as int,
+    );
+  }
+
+
+  @override
+  String toString() => 'Person(name: $name, age: $age)';
+
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    late final Bloc myBloc;
-
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
     );
