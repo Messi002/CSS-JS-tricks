e563e2a6b4177dc45fd3c91927fc53ce4eeb6d1f
Messi002 <rostandmessi2@gmail.com>
2022-11-15 20:43:09 +0100

    bloc

--- DART/app4/lib/main.dart
+++ DART/app4/lib/main.dart
@@ -39,16 +39,6 @@ abstract class LoadPersonAction implements LoadAction {
 
 enum PersonUrl { person1, person2 }
 
-extension UrlString on PersonUrl {
-  String get urlString {
-    switch (this) {
-      case PersonUrl.person1:
-        return 'http://127.0.0.1:5500/app4/api/person1.json';
-      case PersonUrl.person2:
-        return 'http://127.0.0.1:5500/app4/api/person2.json';
-    }
-  }
-}
 
 @immutable
 class Person {
@@ -76,7 +66,10 @@ class Person {
     };
   }
 
-  f
+  factory Person.fromJson(Map<String, dynamic> json) {
+    return Person(
+      name: json['name'] as String,
+      age: json['age'] as int,
     );
   }
 
