307f855dd48074a1e9df875c9851dc12c2e8961e
Messi002 <rostandmessi2@gmail.com>
2022-11-16 21:09:07 +0100

    bloc_training

--- DART/app4/lib/main.dart
+++ DART/app4/lib/main.dart
@@ -12,27 +12,6 @@ extension Log on Object {
   void log() => devtools.log(toString());
 }
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: BlocProvider(
-        create: (_) => PersonBloc(),
-        child: const MyHomePage(),
-      ),
-    );
-  }
-}
 
 @immutable
 abstract class LoadAction {
@@ -148,7 +127,23 @@ class PersonBloc extends Bloc<LoadAction, FetchedResults?> {
 }
 
 class MyHomePage extends StatelessWidget {
-  cons
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    context
+                        .read<PersonBloc>()
+                        .add(const LoadPersonAction(url: PersonUrl.person1));
+                  },
+                  child: const Text("Load json #1")),
               TextButton(
                   onPressed: () {
                     context
