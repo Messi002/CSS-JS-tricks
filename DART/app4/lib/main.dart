a35d53f27a8b6f4242e7ee350736d9bf6987e415
Messi002 <rostandmessi2@gmail.com>
2022-11-16 21:08:50 +0100

    bloc_training

--- DART/app4/lib/main.dart
+++ DART/app4/lib/main.dart
@@ -107,7 +107,19 @@ Future<Iterable<Person>> getPersons(String url) => HttpClient()
     .then((str) => json.decode(str) as List<dynamic>) //future of list
     .then((list) => list.map((e) => Person.fromJson(e))); //future of iterable
 
+@immutable
+class FetchedResults {
+  final Iterable<Person> persons;
+  final bool isRetrievedFromCache;
+  const FetchedResults({
+    required this.persons,
+    required this.isRetrievedFromCache,
+  });
 
+  @override
+  String toString() =>
+      'FetchResult (isRetrievedFromCahce = $isRetrievedFromCache, persons = $persons';
+}
 
 class PersonBloc extends Bloc<LoadAction, FetchedResults?> {
   final Map<PersonUrl, Iterable<Person>> _cache = {};
@@ -136,23 +148,7 @@ class PersonBloc extends Bloc<LoadAction, FetchedResults?> {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              TextButton(
-                  onPressed: () {
-                    context
-                        .read<PersonBloc>()
-                        .add(const LoadPersonAction(url: PersonUrl.person1));
-                  },
-                  child: const Text("Load json #1")),
+  cons
               TextButton(
                   onPressed: () {
                     context
