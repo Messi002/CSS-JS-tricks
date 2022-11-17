6983b1ab74dcfadb33f972c77da2faaabc8e3ffd
Messi002 <rostandmessi2@gmail.com>
2022-11-17 22:34:31 +0100

    bloc

---
+++ DART/app4/lib/main2.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+
+enum PersonsUrl{person1, person2}
+
+extension UrlString on PersonsUrl{
+    
+}
+
+
+
+
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
