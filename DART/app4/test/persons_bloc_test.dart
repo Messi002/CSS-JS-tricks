d6663698a614d46cadb3343baaf9b68740268170
Messi002 <rostandmessi2@gmail.com>
2022-11-18 20:09:27 +0100

    bloc

---
+++ DART/app4/test/persons_bloc_test.dart
@@ -0,0 +1,14 @@
+import 'package:app4/bloc/person.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc_test/bloc_test.dart';
+
+const mockedPersons1 = [
+  Person(age: 20, name: 'Austin'),
+  Person(age:30, name : 'Gareth'),
+];
+
+const mockedPersons2 = [
+  Person(age: 20, name: 'August'),
+  Person(age:30, name : 'Dareth'),
+];
+
