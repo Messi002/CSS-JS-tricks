4a382c7e120bde7b0e2a3a077c6a84aa5e686f93
Messi002 <rostandmessi2@gmail.com>
2022-11-19 23:14:09 +0100

    loginHandle

--- DART/app4/lib/apis/login_api.dart
+++ DART/app4/lib/apis/login_api.dart
@@ -18,11 +18,14 @@ class LoginApi implements LoginProtocolApi {
   static const LoginApi _shared = LoginApi._sharedInstance();
   factory LoginApi.instance() => _shared;
 
-
   @override
-  Future<LoginHandle?> login(
-      {required String email, required String password,}) => 
-
-
-      
+  Future<LoginHandle?> login({
+    required String email,
+    required String password,
+  }) =>
+      Future.delayed(
+              const Duration(seconds: 3),
+              () =>
+                  email == 'austingareth@gmail.com' && password == 'austin123',)
+          .then((isLoggedIn) => isLoggedIn ? const LoginHandle.fooBar() : null);
 }
