67eeac37763ff8f2f5e7ea117a7dc40974fbb062
Messi002 <rostandmessi2@gmail.com>
2022-11-21 19:12:26 +0100

    bloc

--- DART/app4/lib/bloc/app_bloc.dart
+++ DART/app4/lib/bloc/app_bloc.dart
@@ -23,11 +23,33 @@ class AppBloc extends Bloc<AppAction, AppState> {
         final loginHandle =
             await loginApi.login(email: event.email, password: event.password);
 
-        emit(AppState(isLoading: false, loginError: loginHandle == null ? LoginErrors.invalidHandle : null,
-        loginHandle: loginHandle, fetchNotes: null,),);
+        emit(
+          AppState(
+            isLoading: false,
+            loginError: loginHandle == null ? LoginErrors.invalidHandle : null,
+            loginHandle: loginHandle,
+            fetchNotes: null,
+          ),
+        );
       },
     );
     //next action here....
-    on<>
+    on<LoadNotesAction>((event, emit) async {
+      emit(AppState(isLoading: true, loginHandle: state.loginHandle));
+      //get the login handle....
+      final loginHandle = state.loginHandle;
+      if (loginHandle != const LoginHandle.fooBar()) {
+        emit(
+          AppState(
+            isLoading: false,
+            loginError: LoginErrors.invalidHandle,
+            loginHandle: state.loginHandle,
+          ),
+        );
+        return;
+      }
+      //we have a valid login handle and want to fetch notes...
+      final notes = await noteApi.getNotes(loginHandle: loginHandle!);
+    });
   }
 }
