b150dfabc84772c32c084dc82b689926e479cce1
Messi002 <rostandmessi2@gmail.com>
2022-11-21 19:51:56 +0100

    bloc

--- DART/app4/lib/dialogs/generic_dialog.dart
+++ DART/app4/lib/dialogs/generic_dialog.dart
@@ -1,13 +1,24 @@
-
-
 import 'package:flutter/material.dart';
-
 
 typedef DialogOptionBuilder<T> = Map<String, T?> Function();
 
-Future<T?> showGenericDialog({
+Future<T?> showGenericDialog<T>({
   required BuildContext context,
   required String title,
   required String content,
-  required DialogOptionBuilder optionBuilder
-})
+  required DialogOptionBuilder optionsBuilder,
+}) {
+  final options = optionsBuilder();
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions : options.keys.map((optionTitle){
+
+        }).toList(),
+      );
+    },
+  );
+}
