934016ce179d3d62cec913f68a04c2c881244042
Messi002 <rostandmessi2@gmail.com>
2022-11-22 23:14:46 +0100

    bloc_training

--- DART/app4/lib/dialogs/loading_screen.dart
+++ DART/app4/lib/dialogs/loading_screen.dart
@@ -1,6 +1,15 @@
+// ignore_for_file: unnecessary_late
+
+import 'package:app4/dialogs/loading_screen_controller.dart';
+
 class LoadingScreen {
   //creating a singleton
-  const LoadingScreen._sharedInstance();
+   LoadingScreen._sharedInstance();
+  // ignore: prefer_const_constructors
   static late final LoadingScreen _shared = LoadingScreen._sharedInstance();
   factory LoadingScreen.instance() => _shared;
+
+  LoadingScreenController? _controller;
+
+
 }
