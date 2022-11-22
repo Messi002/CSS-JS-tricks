bfc034c950156c18faf96553a9767bc660fa2d8c
Messi002 <rostandmessi2@gmail.com>
2022-11-22 23:40:38 +0100

    bloc_training

--- DART/app4/lib/dialogs/loading_screen.dart
+++ DART/app4/lib/dialogs/loading_screen.dart
@@ -27,37 +27,52 @@ class LoadingScreen {
     final size = renderBox.size;
 
     //displaying here
-    final overlay = OverlayEntry(builder: (context) {
-      return Material(
-        color: Colors.black.withAlpha(150),
-        child: Center(
-          child: Container(
-            constraints: BoxConstraints(
-              maxWidth: size.width * 0.8,
-              maxHeight: size.height * 0.8,
-              minWidth: size.width * 0.5,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Padding(padding: const EdgeInsets.all(16.0),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: <Widget>[
-                  const SizedBox(height: 10),
-                  const CircularProgressIndicator(),
-                  const SizedBox(height: 20),
-                ],
+    final overlay = OverlayEntry(
+      builder: (context) {
+        return Material(
+          color: Colors.black.withAlpha(150),
+          child: Center(
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: size.width * 0.8,
+                maxHeight: size.height * 0.8,
+                minWidth: size.width * 0.5,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: <Widget>[
+                      const SizedBox(height: 10),
+                      const CircularProgressIndicator(),
+                      const SizedBox(height: 20),
+                      StreamBuilder(
+                          stream: _text.stream,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return Text(
+                                snapshot.data!,
+                                textAlign: TextAlign.center,
+                              );
+                            } else {
+                              return Container();
+                            }
+                          })
+                    ],
+                  ),
+                ),
               ),
             ),
-            ),
           ),
-        ),
-      );
-    },);
+        );
+      },
+    );
   }
 }
