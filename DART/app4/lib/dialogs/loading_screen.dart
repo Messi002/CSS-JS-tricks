140ac039b1e31a31559b0b957b91c46f3f50b2ff
Messi002 <rostandmessi2@gmail.com>
2022-11-22 23:18:59 +0100

    bloc_training

--- DART/app4/lib/dialogs/loading_screen.dart
+++ DART/app4/lib/dialogs/loading_screen.dart
@@ -1,15 +1,24 @@
 // ignore_for_file: unnecessary_late
 
+import 'dart:async';
+
 import 'package:app4/dialogs/loading_screen_controller.dart';
+import 'package:flutter/widgets.dart';
 
 class LoadingScreen {
   //creating a singleton
-   LoadingScreen._sharedInstance();
+  LoadingScreen._sharedInstance();
   // ignore: prefer_const_constructors
   static late final LoadingScreen _shared = LoadingScreen._sharedInstance();
   factory LoadingScreen.instance() => _shared;
 
   LoadingScreenController? _controller;
 
-
+  LoadingScreenController showOverlay({
+    required BuildContext context,
+    required String text,
+  }) {
+    final _text = StreamController<String>();
+    _text.add(text);
+  }
 }
