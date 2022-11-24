42c6c91ba7f335da2712e624b4eb5e65e37c2e80
Messi002 <rostandmessi2@gmail.com>
2022-11-24 23:23:08 +0100

    bloc

--- DART/app4/lib/models.dart
+++ DART/app4/lib/models.dart
@@ -3,7 +3,27 @@
 //the user that he's registered or not
 
 
-imn = $token)';
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class LoginHandle {
+  final String token;
+
+  const LoginHandle({
+    required this.token,
+  });
+
+  const LoginHandle.fooBar() : token = 'austingareth';
+
+  @override
+  bool operator ==(covariant LoginHandle other) => token == other.token;
+
+  @override
+  int get hashCode => token.hashCode;
+
+  @override
+  String toString() {
+    return 'LoginHandle (token = $token)';
   }
 }
 
@@ -21,13 +41,4 @@ class Note {
 
   @override
   bool operator ==(covariant Note other) {
-    if (identical(this, other)) return true;
-
-    return other.title == title;
-  }
-
-  @override
-  int get hashCode => title.hashCode;
-}
-
-final mockNotes = Iterable.generate(3, (i) => Note(title: 'Note ${i + 1}'));
+    if (id
