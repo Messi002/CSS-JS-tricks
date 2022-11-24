fb545f4b3a44953d09ccfc5e7a538dc88da351a4
Messi002 <rostandmessi2@gmail.com>
2022-11-24 23:23:36 +0100

    bloc

--- DART/app4/lib/views/iterable_list_view.dart
+++ DART/app4/lib/views/iterable_list_view.dart
@@ -5,3 +5,18 @@ extension ToListView<T> on Iterable<T> {
 }
 
 class IterableListView<T> extends StatelessWidget {
+  final Iterable<T> iterable;
+  const IterableListView({super.key, required this.iterable});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: iterable.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(iterable.elementAt(index).toString()),
+        );
+      },
+    );
+  }
+}
