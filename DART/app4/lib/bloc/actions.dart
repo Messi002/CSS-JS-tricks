52b6c55fe4f2eb466c262e4f83b02f8b7c054abd
Messi002 <rostandmessi2@gmail.com>
2022-11-25 21:38:51 +0100

    bloc

--- DART/app4/lib/bloc/actions.dart
+++ DART/app4/lib/bloc/actions.dart
@@ -6,16 +6,7 @@ abstract class AppAction {
   const AppAction();
 }
 
-@immutable
-class LoginAction implements AppAction {
-  final String email;
-  final String password;
 
-  const LoginAction({
-    required this.email,
-    required this.password,
-  });
-}
 
 @immutable
 class LoadNotesAction implements AppAction {
