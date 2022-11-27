5ac57dcbbdb5e885257a81d8b66e598eef5f7cb2
Messi002 <rostandmessi2@gmail.com>
2022-11-27 21:51:27 +0100

    code restored

--- DART/app4/lib/main.dart
+++ DART/app4/lib/main.dart
@@ -12,7 +12,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
- 
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
