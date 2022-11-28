cb8162259c30333b8f4ac15684fd5d00da4726a7
Messi002 <rostandmessi2@gmail.com>
2022-11-28 21:40:26 +0100

    added check for dialog

--- DART/app4/lib/views/login_button.dart
+++ DART/app4/lib/views/login_button.dart
@@ -1,4 +1,6 @@
+import 'package:app4/dialogs/generic_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:app4/strings.dart';
 
 typedef OnLoginTapped = void Function(
   String email,
@@ -10,10 +12,32 @@ class LoginButton extends StatelessWidget {
   final TextEditingController passwordController;
   final OnLoginTapped onLoginTapped;
 
-  const LoginButton({super.key, required this.emailController, required this.passwordController, required this.onLoginTapped});
+  const LoginButton({
+    super.key,
+    required this.emailController,
+    required this.passwordController,
+    required this.onLoginTapped,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextButton(
+      onPressed: () {
+        final email = emailController.text;
+        final password = passwordController.text;
+
+        if (email.isEmpty || password.isEmpty) {
+          showGenericDialog(
+            context: context,
+            title: emailAndPasswordEmptyDialogTitle,
+            content: emailOrPasswordEmptyDescription,
+            optionsBuilder: () => {
+              ok: true,
+            },
+          );
+        }
+      },
+      child: const Text(login),
+    );
   }
 }
