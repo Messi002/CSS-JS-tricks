30389715091e43bb7f199cb753b8ac4dbe9a5fad
Messi002 <rostandmessi2@gmail.com>
2022-11-28 23:11:46 +0100

    final

--- DART/app4/lib/views/login_view.dart
+++ DART/app4/lib/views/login_view.dart
@@ -1,11 +1,25 @@
+import 'package:app4/views/email_text_field.dart';
+import 'package:app4/views/login_button.dart';
+import 'package:app4/views/password_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class LoginView extends HookWidget {
-  const LoginView({super.key});
+  final OnLoginTapped onLoginTapped;
+  const LoginView(this.onLoginTapped, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final emailController = useTextEditingController();
+    final passwordController = useTextEditingController();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          EmailTextField(emailController: emailController),
+          PasswordTextField(passwordController: passwordController),
+        ],
+      ),
+    );
   }
 }
