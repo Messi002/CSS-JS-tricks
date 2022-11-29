7012c4ada4a3de0937b3aeb4cd21e9012893bf46
Messi002 <rostandmessi2@gmail.com>
2022-11-29 22:30:57 +0100

    flutter_bloc

--- DART/app4/lib/strings.dart
+++ DART/app4/lib/strings.dart
@@ -8,12 +8,4 @@ const loginErrorDialogTitle = 'Login error';
 const loginErrorDialogContent =
     'Invalid email/password combination, Please try again with valid login credentails!';
 
-const pleaseWait = 'Please wait...';
 
-const enterYourPasswordHere = 'Enter your password here...';
-
-const enterYourEmailHere = 'Enter your email here...';
-
-const ok = 'OK';
-const login = 'Log in';
-const homePage = 'Home Page';
