6d83078d609180acc77a55faf2bf4851af0d89eb
Messi002 <rostandmessi2@gmail.com>
2022-11-29 22:32:13 +0100

    aa

--- DART/app4/lib/strings.dart
+++ DART/app4/lib/strings.dart
@@ -1,4 +1,8 @@
+const emailAndPasswordEmptyDialogTitle =
+    'Please fill in both email and the password fields';
 
+const emailOrPasswordEmptyDescription =
+    'You seem to have forgotten to enter either the email or password field, or both';
 
 const loginErrorDialogTitle = 'Login error';
 const loginErrorDialogContent =
