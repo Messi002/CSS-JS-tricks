9fe1f544eea198f16c809a87c38dd51433114128
Messi002 <rostandmessi2@gmail.com>
2022-11-30 23:45:57 +0100

    final changes

--- DART/app4/lib/dialogs/loading_screen_controller.dart
+++ DART/app4/lib/dialogs/loading_screen_controller.dart
@@ -3,3 +3,16 @@ import 'package:flutter/foundation.dart' show immutable;
 
 typedef CloseLoadingScreen = bool Function();
 typedef UpdateLoadingScreen = bool Function(String text);
+
+@immutable
+class LoadingScreenController {
+  final CloseLoadingScreen close;
+  final UpdateLoadingScreen update;
+  
+ const LoadingScreenController({
+    required this.close,
+    required this.update,
+  });
+
+  
+}
