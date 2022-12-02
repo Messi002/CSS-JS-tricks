fef3949d4768f78e442e476623dfa7e6512002f3
Messi002 <rostandmessi2@gmail.com>
2022-12-02 23:44:42 +0100

    dst

--- DART/app4/lib/models.dart
+++ DART/app4/lib/models.dart
@@ -36,16 +36,7 @@ class Note {
     required this.title,
   });
 
-  @override
-  String toString() => 'Note(title: $title)';
-
-  @override
-  bool operator ==(covariant Note other) {
-    if (identical(this, other)) return true;
-
-    return other.title == title;
-  }
-
+ 
   @override
   int get hashCode => title.hashCode;
 }
--- DART/app4/lib/views/email_text_field.dart
+++ DART/app4/lib/views/email_text_field.dart
@@ -2,4 +2,19 @@ import 'package:app4/strings.dart';
 import 'package:flutter/material.dart';
 
 class EmailTextField extends StatelessWidget {
+  final TextEditingController emailController;
 
+  const EmailTextField({super.key, required this.emailController});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+  decoration: const InputDecoration(
+    hintText: enterYourEmailHere,
+  ),
+    );
+  }
+}
