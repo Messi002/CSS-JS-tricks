00d634a246ba7c81fe76acd6acdd5e2f76efdc72
Messi002 <rostandmessi2@gmail.com>
2022-12-06 23:19:49 +0100

    app_state

--- DART/app8/lib/bloc/app_bloc.dart
+++ DART/app8/lib/bloc/app_bloc.dart
@@ -1,8 +1,20 @@
+import 'dart:math' as math;
+
 import 'package:app8/bloc/app_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'bloc_events.dart';
 
+typedef AppBlocRandomUrlPicker = String Function(Iterable<String> allUrls);
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(
+        math.Random().nextInt(length),
+      );
+}
+
 class AppBloc extends Bloc<AppEvent, AppState> {
-  AppBloc() : super(const AppState.empty());
+  AppBloc({
+    AppBlocRandomUrlPicker? urlPicker,
+  }) : super(const AppState.empty());
 }
