a418e86d8fc9693e6aed524437cfbbb0678946a3
Messi002 <rostandmessi2@gmail.com>
2022-12-06 22:49:56 +0100

    app_state

--- DART/app8/lib/bloc/app_state.dart
+++ DART/app8/lib/bloc/app_state.dart
@@ -1,6 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:typed_data';
+
 import 'package:flutter/foundation.dart' show immutable;
 
 @immutable
-class AppState{
+class AppState {
+  final bool isLoading;
+  final Uint8List? data;
+  final Object? error;
 
+  const AppState({
+    required this.isLoading,
+    required this.data,
+    required this.error,
+  });
+
+  const AppState.empty()
+      : isLoading = false,
+        data = null,
+        error = null;
 }
