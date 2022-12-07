91a6cc585fa649a46d86e201e1d6268050189b1e
Messi002 <rostandmessi2@gmail.com>
2022-12-07 22:42:10 +0100

    dart_bloc

--- DART/app8/lib/bloc/app_bloc.dart
+++ DART/app8/lib/bloc/app_bloc.dart
@@ -19,12 +19,7 @@ class AppBloc extends Bloc<AppEvent, AppState> {
   AppBloc({
     required Iterable<String> urls,
     Duration? waitBeforeLoading,
-    AppBlocRandomUrlPicker? urlPicker,
-  }) : super(const AppState.empty()) {
-    on<LoadNextUrlEvent>((event, emit) async {
-      //start loading
-      emit(const AppState(isLoading: true, data: null, error: null));
-      final url = (urlPicker ?? _pickRandomUrl)(urls);
+
 
       try {
         if (waitBeforeLoading != null) {
@@ -32,6 +27,11 @@ class AppBloc extends Bloc<AppEvent, AppState> {
         }
         final bundle = NetworkAssetBundle(Uri.parse(url));
         final data = (await bundle.load(url)).buffer.asUint8List();
+         emit(AppState(
+          isLoading: false,
+          data: data,
+          error: null,
+        ));
       } catch (e) {
         emit(AppState(
           isLoading: false,
