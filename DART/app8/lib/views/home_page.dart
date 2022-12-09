b877238ffc7eced2f23a46996dcaa30bead2f449
Messi002 <rostandmessi2@gmail.com>
2022-12-09 04:37:14 +0100

    app_bloc_view

--- DART/app8/lib/views/home_page.dart
+++ DART/app8/lib/views/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:app8/bloc/bottom_bloc.dart';
+import 'package:app8/bloc/top_bloc.dart';
+import 'package:app8/models/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show SystemUiOverlayStyle;
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,6 +10,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.dark,
+          child: MultiBlocProvider(
+            providers: [
+              BlocProvider<TopBloc>(
+                create: (context) => TopBloc(
+                    waitBeforeLoading: const Duration(seconds: 3),
+                    urls: images),
+              ),
+              BlocProvider<BottomBloc>(
+                create: (context) => BottomBloc(
+                    waitBeforeLoading: const Duration(seconds: 3),
+                    urls: images),
+              ),
+            ],
+            child: Container(),
+          )),
+    );
   }
 }
