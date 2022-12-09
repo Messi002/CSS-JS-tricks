8ee1546f1535311080dfda70a453c72d67511792
Messi002 <rostandmessi2@gmail.com>
2022-12-09 04:16:56 +0100

    app_bloc_view

--- DART/app8/lib/views/app_bloc_view.dart
+++ DART/app8/lib/views/app_bloc_view.dart
@@ -1,7 +1,19 @@
+import 'package:app8/bloc/app_bloc.dart';
+import 'package:app8/bloc/bloc_events.dart';
+import 'package:app8/extensionis/stream/start_with.dart';
+import 'package:flutter/material.dart';
 
-
-class AppBlocView extends StatelessWidget {
+class AppBlocView<T extends AppBloc> extends StatelessWidget {
   const AppBlocView({super.key});
+
+  void startUpdatingBloc(BuildContext context) {
+    Stream.periodic(
+      const Duration(seconds: 10),
+      (_) => const LoadNextUrlEvent(),
+    ).startWith(const LoadNextUrlEvent()).forEach((element) {
+      context.read<T>().add(event);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
