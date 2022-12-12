0f5b6d1ab617f9e778143008e27c19b7df8dd80b
Messi002 <rostandmessi2@gmail.com>
2022-12-12 23:17:40 +0100

    added task

--- DART/app4/lib/views/iterable_list_view.dart
+++ DART/app4/lib/views/iterable_list_view.dart
@@ -8,5 +8,15 @@ class IterableListView<T> extends StatelessWidget {
   final Iterable<T> iterable;
   const IterableListView({super.key, required this.iterable});
 
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: iterable.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(iterable.elementAt(index).toString()),
+        );
+      },
+    );
   }
 }
