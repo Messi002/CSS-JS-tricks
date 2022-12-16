a93876fd5dbb73c84afcfc5ec6fc438c1185b33e
Messi002 <rostandmessi2@gmail.com>
2022-12-16 23:28:07 +0100

    dart_firebase

--- DART/app5/lib/main.dart
+++ DART/app5/lib/main.dart
@@ -31,6 +31,15 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+     
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
