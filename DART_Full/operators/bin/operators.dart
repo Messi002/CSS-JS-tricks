63f594f9b0c36148e2a8adab6c6f65d25330ef89
Messi002 <rostandmessi2@gmail.com>
2022-12-18 15:05:14 +0100

    operators_simple_integer_operators

--- DART_Full/operators/bin/operators.dart
+++ DART_Full/operators/bin/operators.dart
@@ -1,12 +1,16 @@
 import 'package:operators/operators.dart' as operators;
 
 void main(List<String> arguments) {
-  late final value = getValue();
-  print("We are here");
-  print("$value");
+  // late final int value = getValue();
+  // print("We are here");
+  // print("$value");
+  final age1 = 20;
+  final age2 = 30;
+  //This is a compound operator
+  print(age1 + age2 +30);
 }
 
-int getValue() {
-  print("being called from the function");
-  return 10;
-}
+// int getValue() {
+//   print("being called from the function");
+//   return 10;
+// }
