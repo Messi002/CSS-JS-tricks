975afad68cc58944840afcefb4b91762e444e8dd
Messi002 <rostandmessi2@gmail.com>
2022-12-18 22:46:58 +0100

    app8_firebaseException

---
+++ DART/app5/lib/auth/auth_error.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuthException;
+
+const Map<String, AuthError> authErrorMapping = {};
+
+@immutable
+abstract class AuthError {
+  final String dialogTitle;
+  final String dialogText;
+
+  const AuthError({
+    required this.dialogTitle,
+    required this.dialogText,
+  });
+
+  factory AuthError.from(FirebaseAuthException exception) =>
+      authErrorMapping[exception.code.toLowerCase().trim()] ??
+      const AuthErrorUnknown();
+}
+
+@immutable
+class AuthErrorUnknown extends AuthError {
+  const AuthErrorUnknown()
+      : super(
+            dialogText: 'Authentication error',
+            dialogTitle: 'Unknown authentication error');
+}
