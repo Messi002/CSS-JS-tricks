c4be6c13cfdce1241621c210c868224d4f3fd2dc
Messi002 <rostandmessi2@gmail.com>
2022-12-18 15:27:54 +0100

    operators_bitwise

--- DART_Full/operators/bin/operators.dart
+++ DART_Full/operators/bin/operators.dart
@@ -4,13 +4,28 @@ void main(List<String> arguments) {
   // late final int value = getValue();
   // print("We are here");
   // print("$value");
-  final age1 = 20;
-  final age2 = 30;
+  final age1 = 64;
+  final age2 = 20;
   //This is a compound operator
-  print(age1 + age2 +30);
+  print(age1 + age2 + 30);
+  //This is the truncating operator which returns an int
+  //rather than a double like would do a normal division
+  final intDivided = age1 ~/ age2;
+  print(intDivided);
+
+  ///Unary_prefix
+  var age3 = 30;
+
+  ///This decreases the value and returns it and decreases the
+  ///age3 itself
+  print(--age3);
+
+  ///Unary bitwise complement prefix operator
+  print(~1);
 }
 
 // int getValue() {
 //   print("being called from the function");
 //   return 10;
 // }
+
