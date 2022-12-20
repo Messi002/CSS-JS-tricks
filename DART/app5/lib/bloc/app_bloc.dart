587c31fd3c57ea4be072bfc22dab13763f3ffeaa
Messi002 <rostandmessi2@gmail.com>
2022-12-21 00:10:44 +0100

    still_in_appbloc

--- DART/app5/lib/bloc/app_bloc.dart
+++ DART/app5/lib/bloc/app_bloc.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:app5/auth/auth_error.dart';
-import 'package:bloc/bloc.dart';
+import 'package:app5/utils/upload_image.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart' show immutable;
@@ -9,9 +12,38 @@ part 'app_event.dart';
 part 'app_state.dart';
 
 class AppBloc extends Bloc<AppEvent, AppState> {
-  AppBloc() : super(AppInitial()) {
-    on<AppEvent>((event, emit) {
-      // TODO: implement event handler
+  AppBloc()
+      : super(const AppStateLoggedOut(
+          isLoading: false,
+        )) {
+    //handle uploading images
+    on<AppEventUploadImage>((event, emit) async {
+      final user = state.user;
+//log user out if we don't have an actual user in the app state
+      if (user == null) {
+        emit(const AppStateLoggedOut(isLoading: false));
+        return;
+      }
+//Emitting a loading state...
+      emit(AppStateLoggedIn(
+        isLoading: true,
+        user: user,
+        images: state.images ?? [],
+      ));
+
+//Uploading
+      final file = File(event.filePathToUpload);
+
+      await uploadImage(file: file, userId: user.uid);
+//After uploading file is complete, grab the latest files
+      final images = await _getImages(user.uid);
+      
     });
   }
+
+  Future<Iterable<Reference>> _getImages(String userId) =>
+      FirebaseStorage.instance
+          .ref(userId)
+          .list()
+          .then((listResult) => listResult.items);
 }
--- DART/app5/lib/bloc/app_state.dart
+++ DART/app5/lib/bloc/app_state.dart
@@ -1,6 +1,30 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'app_bloc.dart';
 
+extension GetUser on AppState {
+  User? get user {
+    final cls = this;
+    if (cls is AppStateLoggedIn) {
+      return cls.user;
+    } else {
+      return null;
+    }
+  }
+}
+
+//This is to be able to grab the images of user instance from any 
+//appstate be it AppStateLoggedOut or AppStateLoggedIn
+extension GetImage on AppState {
+  Iterable<Reference>? get images {
+    final cls = this;
+    if (cls is AppStateLoggedIn) {
+      return cls.images;
+    } else {
+      return null;
+    }
+  }
+}
+
 @immutable
 abstract class AppState {
   final bool isLoading;
@@ -12,6 +36,7 @@ abstract class AppState {
   });
 }
 
+@immutable
 class AppStateLoggedIn extends AppState {
   final User user;
   final Iterable<Reference> images;
@@ -20,9 +45,59 @@ class AppStateLoggedIn extends AppState {
     required bool isLoading,
     required this.user,
     required this.images,
+  AuthError? authError,
+  }) : super(
+          isLoading: isLoading,
+          authError: authError,
+        );
+
+  @override
+  bool operator ==(other) {
+    final otherClass = other;
+    if (otherClass is AppStateLoggedIn) {
+      //As long as these are thesame then the instances are of the same
+      //type
+      return isLoading == otherClass.isLoading &&
+          user.uid == otherClass.user.uid &&
+          images.length == otherClass.images.length;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  int get hashCode => Object.hash(user.uid, images);
+
+  @override
+  String toString() => 'AppStateLoggedIn images_length = ${images.length}';
+}
+
+@immutable
+class AppStateLoggedOut extends AppState {
+  const AppStateLoggedOut({
+    required bool isLoading,
+    AuthError? authError,
+  }) : super(
+          isLoading: isLoading,
+          authError: authError,
+        );
+
+  @override
+  String toString() =>
+      'AppStateLoggedOut isLoading = $isLoading, authError = $authError';
+}
+
+@immutable
+class AppStateIsInRegistrationView extends AppState {
+  const AppStateIsInRegistrationView({
+    required bool isLoading,
     required AuthError? authError,
   }) : super(
           isLoading: isLoading,
           authError: authError,
         );
+
+  @override
+  String toString() =>
+      'AppStateIsInRegistrationView isLoading = $isLoading, authError = $authError';
 }
