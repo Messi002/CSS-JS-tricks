b84f03458509f5231d27eb0035e9898d3caef9b1
Messi002 <rostandmessi2@gmail.com>
2022-12-21 18:18:19 +0100

    colors

--- DART/app5/lib/utils/app_colors.dart
+++ DART/app5/lib/utils/app_colors.dart
@@ -2,17 +2,91 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   //Primary Colors
-  static const Color primaryColor500 = Color.fromRGBO(255, 245, 16, 1);
+  static const Color primaryColor500 = Color(0xFFFFF510);
 
-  static const Color primaryColor400 = Color.fromRGBO(255, 247, 64, 1);
+  static const Color primaryColor400 = Color(0xFFFFF740);
 
-  static const Color primaryColor300 = Color.fromRGBO(255, 249, 113, 1);
+  static const Color primaryColor300 = Color(0xFFFFF971);
 
-  static const Color primaryColor200 = Color.fromRGBO(255, 251, 169, 1);
+  static const Color primaryColor200 = Color(0xFFFFFBA9);
 
-  static const Color primaryColor100 = Color.fromRGBO(255, 253, 218, 1);
+  static const Color primaryColor100 = Color(0xFFFFFDDA);
 
-  static const Color primaryColor50 = Color.fromRGBO(255, 254, 236, 1);
+  static const Color primaryColor50 = Color(0xFFFFFEEC);
+
+  //Primay Shades Colors
+  static const Color primaryColor700 = Color(0xFF98920A);
+
+  static const Color primaryColor800 = Color(0xFF7A7508);
+
+  static const Color primaryColor900 = Color(0xFF5C5806);
+
+  static const Color primaryColor600 = Color(0xFF594C16);
+
+  //Grey Colors
+  static const Color greyBlack = Color(0xFF000000);
+
+  static const Color grey900 = Color(0xFF3D3B33);
+
+  static const Color grey800 = Color(0xFF6E6C66);
+
+  static const Color grey700 = Color(0xFFA3A199);
+
+  static const Color grey600 = Color(0xFFD1D0CC);
+
+  static const Color grey500 = Color(0xFFE8E8E5);
+
+  static const Color greyWhite = Color(0xFFFFFFFF);
+
+  //Accent Colors
+  static const Color accentColor700 = Color(0xFF210E2B);
+
+  static const Color accentColor600 = Color(0xFF291235);
+
+  static const Color accentColor500 = Color(0xFF361847);
+
+  static const Color accentColor400 = Color(0xFF441E59);
+
+  static const Color accentColor300 = Color(0xFF694B7A);
+
+  static const Color accentColor200 = Color(0xFF90799C);
+
+  static const Color accentColor100 = Color(0xFFBCAEC3);
+
+  static const Color accentColor50 = Color(0xFFF0EDF2);
+
+  //Semantic Colors
+  static const Color semanticColor600 = Color(0xFFFF002E);
+
+  static const Color semanticColor500 = Color(0xFFFF3B5E);
+
+  static const Color semanticColor400 = Color(0xFFFC7B92);
+
+  static const Color semanticColor300 = Color(0xFFFF6711);
+
+  static const Color semanticColor200 = Color(0xFFFF8641);
+
+  static const Color semanticColor100 = Color(0xFFFFAD7F);
+
+  //Gradient Color
+  static const Color gradientColor1 = Color(0xFFF2B908);
+
+  static const Color gradientColor2 = Color(0xFFF8D33C);
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+ 
 
 
  
