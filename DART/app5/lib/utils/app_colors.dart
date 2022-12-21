ff0c25c558155d933ba757374fabd6543e014c6b
Messi002 <rostandmessi2@gmail.com>
2022-12-21 17:44:05 +0100

    colors

---
+++ DART/app5/lib/utils/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Primary Colors
+  static const Color primaryColor500 = Color.fromRGBO(255, 245, 16, 1);
+
+  static const Color primaryColor400 = Color.fromRGBO(255, 247, 64, 1);
+
+  static const Color primaryColor300 = Color.fromRGBO(255, 249, 113, 1);
+
+  static const Color primaryColor200 = Color.fromRGBO(255, 251, 169, 1);
+
+  static const Color primaryColor100 = Color.fromRGBO(255, 253, 218, 1);
+
+  static const Color primaryColor50 = Color.fromRGBO(255, 254, 236, 1);
+
+
+ 
+
+  
+ 
+
+ 
+}
