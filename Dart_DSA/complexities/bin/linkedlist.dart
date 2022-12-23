916be2f067f93728a922a853b2e5df731251f64a
Messi002 <rostandmessi2@gmail.com>
2022-12-23 15:43:32 +0100

    work

--- Dart_DSA/complexities/bin/linkedlist.dart
+++ Dart_DSA/complexities/bin/linkedlist.dart
@@ -1,9 +1,26 @@
-
 class Node<T> {
   T value;
   Node<T>? next;
 
-   Node({
-    required this.value, required this.next,
+  Node({
+    required this.value,
+    this.next,
   });
+
+  @override
+  String toString() {
+    if (next == null) return 'value: $value -> null';
+    return 'value: $value -> ${next.toString()}';
+  }
+}
+
+void main(List<String> args) {
+  final node1 = Node<int>(value: 1);
+  final node2 = Node<int>(value: 2);
+  final node3 = Node<int>(value: 3);
+
+  node1.next = node2;
+  node2.next = node3;
+
+  print(node1);
 }
