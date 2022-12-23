fd7e7942025b16447c6f38c2274784b1096947b6
Messi002 <rostandmessi2@gmail.com>
2022-12-23 20:37:39 +0100

    linked_list_push

--- Dart_DSA/complexities/bin/linkedlist.dart
+++ Dart_DSA/complexities/bin/linkedlist.dart
@@ -9,18 +9,46 @@ class Node<T> {
 
   @override
   String toString() {
-    if (next == null) return 'value: $value -> null';
-    return 'value: $value -> ${next.toString()}';
+    if (next == null) return '$value -> null';
+    return '$value -> ${next.toString()}';
+  }
+}
+
+class LinkedList<T> {
+  Node<T>? head;
+  Node<T>? tail;
+
+  LinkedList({this.head, this.tail});
+
+  bool get isEmpty => head == null;
+
+  void push(T value) {
+    head = Node(value: value, next: head);
+    tail ??= head;
+  }
+
+  @override
+  String toString() {
+    if (isEmpty) return 'empty linked_list';
+    return head.toString();
   }
 }
 
 void main(List<String> args) {
-  final node1 = Node<int>(value: 1);
-  final node2 = Node<int>(value: 2);
-  final node3 = Node<int>(value: 3);
+  // final node1 = Node(value: 1);
+  // final node2 = Node(value: 2);
+  // final node3 = Node(value: 3);
 
-  node1.next = node2;
-  node2.next = node3;
+  // node1.next = node2;
+  // node2.next = node3;
 
-  print(node1);
+  // print(node1);
+
+  final linkedlist = LinkedList<int>();
+
+  linkedlist.push(3);
+  linkedlist.push(3);
+  linkedlist.push(3);
+
+  print(linkedlist);
 }
