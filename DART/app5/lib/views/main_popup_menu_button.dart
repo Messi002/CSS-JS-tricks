866108a4ccdf3e5d7d58571713707a0316394030
Messi002 <rostandmessi2@gmail.com>
2022-12-27 20:59:28 +0100

    bloc

--- DART/app5/lib/views/main_popup_menu_button.dart
+++ DART/app5/lib/views/main_popup_menu_button.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 
-enum MenuAction {logout, deleteAccount}
+enum MenuAction { logout, deleteAccount }
 
 class MainPopupMenuButton extends StatelessWidget {
   const MainPopupMenuButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ();
+    return PopupMenuButton<MenuAction>(
+      onSelected: (value) {
+        switch (value){
+          
+          case MenuAction.logout:
+            // TODO: Handle this case.
+            break;
+          case MenuAction.deleteAccount:
+            // TODO: Handle this case.
+            break;
+        }
+      },
+      itemBuilder: (BuildContext context) {
+        return [
+         const PopupMenuItem<MenuAction>(
+            value: MenuAction.logout,
+            child: Text('Log Out'),),
+          const PopupMenuItem<MenuAction>(
+            value: MenuAction.deleteAccount,
+            child: Text('Delete Account'),),
+        ];
+      },
+    );
   }
 }
