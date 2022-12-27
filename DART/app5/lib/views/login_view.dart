ee96bb25ff75722ef9fbf3d5e8d76fdd18139d21
Messi002 <rostandmessi2@gmail.com>
2022-12-27 16:24:44 +0100

    bloc

---
+++ DART/app5/lib/views/login_view.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class LoginView extends HookWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
