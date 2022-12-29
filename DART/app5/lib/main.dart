e85e6b4b455f2eb82c6c01064cb028c496fd4436
Messi002 <rostandmessi2@gmail.com>
2022-12-29 09:22:25 +0100

    bloc

--- DART/app5/lib/main.dart
+++ DART/app5/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:app5/views/app.dart';
 import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- DART/app5/lib/views/app.dart
+++ DART/app5/lib/views/app.dart
@@ -1,4 +1,11 @@
+import 'package:app5/bloc/app_bloc.dart';
+import 'package:app5/dialogs/loading_screen.dart';
+import 'package:app5/dialogs/show_auth_error.dart';
+import 'package:app5/views/login_view.dart';
+import 'package:app5/views/photo_gallery_view.dart';
+import 'package:app5/views/registration_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -6,12 +13,43 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (_) => AppBloc()..add(const AppEventInitialize()),
+      child: MaterialApp(
+        title: 'Photo Library',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: BlocConsumer<AppBloc, AppState>(
+          listener: (context, appState) {
+            if (appState.isLoading) {
+              LoadingScreen.instance().show(
+                context: context,
+                text: 'Loading...',
+              );
+            } else {
+              LoadingScreen.instance().hide();
+            }
+
+            final authError = appState.authError;
+            if (authError != null) {
+              showAuthError(
+                authError: authError,
+                context: context,
+              );
+            }
+          },
+          builder: (context, appState) {
+            if (appState is AppStateLoggedIn) return const PhotoGalleryView();
+            if (appState is AppStateLoggedOut) return const LoginView();
+            if (appState is AppStateIsInRegistrationView)
+              return const RegisterView();
+            //this should never happen
+            return Container();
+          },
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
