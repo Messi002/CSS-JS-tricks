4da36ed075465842c5bbf6d15cc30881a6e5d742
Messi002 <rostandmessi2@gmail.com>
2022-12-29 08:53:25 +0100

    bloc

--- DART/app5/lib/views/photo_gallery_view.dart
+++ DART/app5/lib/views/photo_gallery_view.dart
@@ -1,3 +1,5 @@
+import 'package:app5/views/main_popup_menu_button.dart';
+import 'package:app5/views/storage_image_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -12,6 +14,36 @@ class PhotoGalleryView extends HookWidget {
   Widget build(BuildContext context) {
     final picker = useMemoized(() => ImagePicker(), [key]);
     final images = context.watch<AppBloc>().state.images ?? [];
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Photo Gallery'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final XFile? image =
+                  await picker.pickImage(source: ImageSource.gallery);
+              if (image == null) return;
+              // ignore: use_build_context_synchronously
+              context.read<AppBloc>().add(
+                    AppEventUploadImage(filePathToUpload: image.path),
+                  );
+            },
+            icon: const Icon(Icons.upload),
+          ),
+          const MainPopupMenuButton(),
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(8),
+        mainAxisSpacing: 20.0,
+        crossAxisSpacing: 20.0,
+        children: images
+            .map(
+              (img) => StorageImageView(image: img),
+            )
+            .toList(),
+      ),
+    );
   }
 }
