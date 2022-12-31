6f7f4eb3865bf289b2161c1a45660002442592f2
Messi002 <rostandmessi2@gmail.com>
2022-12-31 20:40:35 +0100

    bloc

--- DART/app5/lib/auth/auth_error.dart
+++ DART/app5/lib/auth/auth_error.dart
@@ -93,16 +93,6 @@ class AuthErrorWeakPassword extends AuthError {
 }
 
 @immutable
-class AuthErrorInvalidEmail extends AuthError {
-  const AuthErrorInvalidEmail()
-      : super(
-          dialogTitle: 'Invalid email',
-          dialogText:
-              'Please double check your email and try again',
-        );
-}
-
-@immutable
 class AuthErrorEmailAlreadyInUse extends AuthError {
   const AuthErrorEmailAlreadyInUse()
       : super(
