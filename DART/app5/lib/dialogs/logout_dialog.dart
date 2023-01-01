bf511eace61c7761c3822fdb8e0499dbcf27b886
Messi002 <rostandmessi2@gmail.com>
2023-01-01 21:13:07 +0100

    bloc

--- DART/app5/lib/dialogs/logout_dialog.dart
+++ DART/app5/lib/dialogs/logout_dialog.dart
@@ -1,4 +1,12 @@
 import 'package:app5/dialogs/generic_dialog.dart';
 import 'package:flutter/material.dart' show BuildContext;
 
-
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showGenericDialog<bool>(
+      context: context,
+      title: 'Log Out',
+      content:
+          "Are you sure you want to log out?",
+      optionsBuilder: () => {'Cancel' : false,
+      'Log Out' : true},).then((value) => value ?? false);
+}
