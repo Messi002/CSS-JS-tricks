4552fba2ca9893b9c9f590c1c81ec928850a34c1
Messi002 <rostandmessi2@gmail.com>
2023-01-05 22:42:47 +0100

    queues_added

--- Dart_DSA/complexities/bin/trees.dart
+++ Dart_DSA/complexities/bin/trees.dart
@@ -8,6 +8,46 @@ class TreeNode<T> {
   }
 
   void forEachDepthFirst(
-    void Function(TreeNode<T> node) performAction
-  )
+    void Function(TreeNode<T> node) performAction,
+  ) {
+    performAction(this);
+    for (var child in children) {
+      child.forEachDepthFirst(performAction);
+    }
+  }
 }
+
+TreeNode<String> makeBeverageTree() {
+  final tree = TreeNode('beverages');
+  final hot = TreeNode('hot');
+  final cold = TreeNode('cold');
+  final tea = TreeNode('tea');
+  final coffee = TreeNode('coffee');
+  final chocolate = TreeNode('cocoa');
+  final blackTea = TreeNode('black');
+  final greenTea = TreeNode('green');
+  final chaiTea = TreeNode('chai');
+  final soda = TreeNode('soda');
+  final milk = TreeNode('milk');
+  final gingerAle = TreeNode('ginger ale');
+  final bitterLemon = TreeNode('bitter lemon');
+  tree.add(hot);
+  tree.add(cold);
+  hot.add(tea);
+  hot.add(coffee);
+  hot.add(chocolate);
+  cold.add(soda);
+  cold.add(milk);
+  tea.add(blackTea);
+  tea.add(greenTea);
+  tea.add(chaiTea);
+  soda.add(gingerAle);
+  soda.add(bitterLemon);
+  return tree;
+}
+
+void main(List<String> args) {
+  final tree = makeBeverageTree();
+  tree.forEachDepthFirst((node) => print(node.value));
+}
+//Pg 106
