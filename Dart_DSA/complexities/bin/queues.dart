3ade09244686883debf2ba13a0f2f50c4d5128da
Messi002 <rostandmessi2@gmail.com>
2023-01-05 20:47:27 +0100

    queues_added

--- Dart_DSA/complexities/bin/queues.dart
+++ Dart_DSA/complexities/bin/queues.dart
@@ -1,1 +1,30 @@
+abstract class Queue<E> {
+  bool enqueue(E element);
+  E? dequeue();
+  bool? isEmpty;
+  E? peek();
+}
 
+class QueueList<E> implements Queue<E> {
+  final _list = <E>[];
+  @override
+  bool? isEmpty;
+
+  @override
+  E? dequeue() {
+    // TODO: implement dequeue
+    throw UnimplementedError();
+  }
+
+  @override
+  bool enqueue(E element) {
+    // TODO: implement enqueue
+    throw UnimplementedError();
+  }
+
+  @override
+  E? peek() {
+    // TODO: implement peek
+    throw UnimplementedError();
+  }
+}
