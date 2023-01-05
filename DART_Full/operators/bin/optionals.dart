46e94f573310bf278b8d8d41defa6657c01e6fa1
Messi002 <rostandmessi2@gmail.com>
2023-01-05 05:02:50 +0100

    added_code

--- DART_Full/operators/bin/optionals.dart
+++ DART_Full/operators/bin/optionals.dart
@@ -13,5 +13,17 @@ void main(List<String> args) {
   List<String?>? names;
   final String? firstName = names?.first;
   print(firstName ?? 'No names found');
-  
 }
+
+String getFullName(
+  String? firstName,
+  String? lastName,
+) =>
+    withAll([firstName, lastName], (names) => names.join(' ')) ??
+    'Empty';
+
+T? withAll<T>(
+  List<T?> optionals,
+  T Function(List<T>) callback,
+) =>
+    optionals.any((e) => e == null) ? null : callback(optionals.cast<T>());
