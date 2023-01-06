b2e62edd2ee905ae1b2be03e5bc770305f34d696
Messi002 <rostandmessi2@gmail.com>
2023-01-06 05:02:04 +0100

    added_code

--- DART_Full/operators/bin/late_keyword.dart
+++ DART_Full/operators/bin/late_keyword.dart
@@ -2,10 +2,16 @@
 //Late variables are initialised when they are first used...
 
 void main(List<String> args) {
-  final person = Person();
-  print(person.fullNames);
-  print(person.fullNames);
-  print(person.fullNames);
+  // final person = Person();
+  // print(person.fullNames);
+  // print(person.fullNames);
+
+  final woof = Dog();
+  woof.description = 'Description 1';
+  print(woof.description);
+  //Can't be assigned again application will crash...
+  // woof.description = 'Description 2';
+  // print(woof.description);
 }
 
 //Even values in Classes are initialized only when
@@ -28,10 +34,24 @@ void main(List<String> args) {
 ///Chaining late variables
 class Person {
   late String fullNames = _getFullNames();
-  // late String firstName = fullNames.split
+  late String firstName = fullNames.split(' ').first;
+  late String lastName = fullNames.split(' ').last;
 
   String _getFullNames() {
     print('Function _getFullNames is called');
     return 'Foo Baz';
   }
+}
+
+///Late keyword with the final keyword
+class Dog {
+  //late final variables can't be assigned twice
+  late final String description;
+}
+
+///late variables depend on late final variables
+class Person1 {
+  late final String firstName;
+  late final String lastName;
+  late String fullNames = '$firstName $lastName';
 }
