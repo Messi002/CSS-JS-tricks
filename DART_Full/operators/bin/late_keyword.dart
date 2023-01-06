8cd2cd0c66d8c7d66597279cb570e656da849375
Messi002 <rostandmessi2@gmail.com>
2023-01-06 04:40:26 +0100

    added_code

--- DART_Full/operators/bin/late_keyword.dart
+++ DART_Full/operators/bin/late_keyword.dart
@@ -1,18 +1,37 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 //Late variables are initialised when they are first used...
 
-void main(List<String> args) {}
+void main(List<String> args) {
+  final person = Person();
+  print(person.fullNames);
+  print(person.fullNames);
+  print(person.fullNames);
+}
 
+//Even values in Classes are initialized only when
+//in use
+// class Person {
+//   late String description = heavyCalculation();
+//   final int age;
+//   Person({
+//     this.age = 18,
+//   }) {
+//     print('Constructor is called');
+//   }
+
+//   String heavyCalculation() {
+//     print('Function heavyCalculation is called');
+//     return 'Foo Baz';
+//   }
+// }
+
+///Chaining late variables
 class Person {
-  final int age;
-  Person({
-    this.age = 18,
-  }) {
-    print('Constructor is called');
-  }
+  late String fullNames = _getFullNames();
+  // late String firstName = fullNames.split
 
-  String heavyCalculation() {
-    print('Function heavyCalculation is called');
+  String _getFullNames() {
+    print('Function _getFullNames is called');
     return 'Foo Baz';
   }
 }
