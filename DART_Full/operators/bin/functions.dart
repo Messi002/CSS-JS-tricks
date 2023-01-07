34afcc9c19bd8e824538d03ee167ab0db7f033c2
Messi002 <rostandmessi2@gmail.com>
2023-01-07 03:30:30 +0100

    added_code

--- DART_Full/operators/bin/functions.dart
+++ DART_Full/operators/bin/functions.dart
@@ -5,8 +5,9 @@
 
 void main(List<String> args) {
   describe();
-  describe(something: null); //here the return type isn't used but null is printed
-  describe(something: 'Hello, Dart!');//here the string passed is used...
+  describe(
+      something: null); //here the return type isn't used but null is printed
+  describe(something: 'Hello, Dart!'); //here the string passed is used...
 }
 
 //An optional value having a default value
@@ -15,3 +16,25 @@ void describe({
 }) {
   print(something);
 }
+
+//Named required parameters can be optional but can't have default values...
+//Named parameters can appear in any order...
+
+//Positional paramters are associated with order and don't have names associated with them
+//always required and cannot have defualt values
+void sayGoodbyeTo(String person, String andOtherPerson) =>
+    print("Goodbye to $person and $andOtherPerson, thanks for coming");
+
+//Optional positional parameters and if default values are needed then [] are
+//needed
+void makeUpperCase([
+  String? name, //This is an optional optional paramter
+  String lastName = 'Baz',
+]) {
+  print(name?.toUpperCase());
+  print(lastName.toUpperCase());
+}
+
+//Matching different styles
+void describeFully(String firstName, {String? lastName = 'Bar'}) =>
+    print('Hello $firstName $lastName');
