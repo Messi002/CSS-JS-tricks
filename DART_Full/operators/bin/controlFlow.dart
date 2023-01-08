7d790289419544bd04a24401b07a0376bcaff27c
Messi002 <rostandmessi2@gmail.com>
2023-01-08 05:43:19 +0100

    added_code

--- DART_Full/operators/bin/controlFlow.dart
+++ DART_Full/operators/bin/controlFlow.dart
@@ -14,7 +14,7 @@
 //Grabbing an index from the modern for loop
 void main(List<String> args) {
   final names = ['Paul, "Geroge', "Easy"];
-  int counter = 1;
+  int counter = 0;
   for (var number in Iterable.generate(10)) {
     print(number);
   }
@@ -26,12 +26,34 @@ void main(List<String> args) {
   }
   // or
   int countera = -1;
-  while (++countera < names.length) {
-    
-  }
+  while (++countera < names.length) {}
+
+//D0-While executes first the body before checking on the condition
+  do {
+    print(names[counter++]);
+  } while (counter < names.length);
+
+  //
+  const info = {
+    'name' : 'John',
+    'age' : 30,
+    'height' : 1.8,
+    'isMarried' : false,
+    'address' : {
+      'street' : 'main street',
+      'city' : 'Yaounde',
+      'country' : 'Cameroon'
+    }
+  };
 }
 
-//While loop evaluates just the condition and if that condition is met, then the 
+//While loop evaluates just the condition and if that condition is met, then the
 //loop is exited i.e. it evaluates first the condition then executes its body
 //While can be read "as long as"
 
+//Switch cases
+
+void describe<T>(T value) {
+  switch (T) {
+  }
+}
