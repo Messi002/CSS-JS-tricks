11e93ff8f77649ae2bf1ecb6df52dc13112f1de9
Messi002 <rostandmessi2@gmail.com>
2023-01-08 04:37:42 +0100

    added_code

--- DART_Full/operators/bin/controlFlow.dart
+++ DART_Full/operators/bin/controlFlow.dart
@@ -3,4 +3,10 @@
 //Control flow statements are used to change the flow of the application
 //E.g. if, else ifs, 
 
-//traditional for loops
+//traditional for loops(counting from start to end, counting from end to start,
+//skipping some values by i +=2) using an array
+
+//Modern for loop
+//for(name in names) => print(name);
+
+//Continue : jump over meaning go back to the start of the loop
