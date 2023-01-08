a1bcfd1aeb2289d403ebef884d2169d6705d9837
Messi002 <rostandmessi2@gmail.com>
2023-01-08 05:25:21 +0100

    added_code

--- DART_Full/operators/bin/controlFlow.dart
+++ DART_Full/operators/bin/controlFlow.dart
@@ -1,7 +1,5 @@
-
-
 //Control flow statements are used to change the flow of the application
-//E.g. if, else ifs, 
+//E.g. if, else ifs,
 
 //traditional for loops(counting from start to end, counting from end to start,
 //skipping some values by i +=2) using an array
@@ -9,4 +7,31 @@
 //Modern for loop
 //for(name in names) => print(name);
 
-//Continue : jump over meaning go back to the start of the loop
+//Continue : meaning goes back to the execution of the for loop
+
+//Break: Breaks completely out of the loop
+
+//Grabbing an index from the modern for loop
+void main(List<String> args) {
+  final names = ['Paul, "Geroge', "Easy"];
+  int counter = 1;
+  for (var number in Iterable.generate(10)) {
+    print(number);
+  }
+
+  //while loop
+  while (counter < names.length) {
+    print(names);
+    counter++;
+  }
+  // or
+  int countera = -1;
+  while (++countera < names.length) {
+    
+  }
+}
+
+//While loop evaluates just the condition and if that condition is met, then the 
+//loop is exited i.e. it evaluates first the condition then executes its body
+//While can be read "as long as"
+
