db5c1be18c159989de938220bc8d2d3873e05193
Messi002 <rostandmessi2@gmail.com>
2023-01-10 13:29:33 +0100

    collection_sets

--- DART_Full/operators/bin/collections_sets.dart
+++ DART_Full/operators/bin/collections_sets.dart
@@ -1,9 +1,36 @@
-
+//download the collection package for sets i.e. dart pub add collection
+// import 'package:collection/collection.dart';
 
 void main(List<String> args) {
-    // print('===================');
+  final names = {
+    'Foo',
+    'Bar',
+    'Baz',
+    'Qux',
+  };
+  // print('===================');
+  final names2 = [
+    'Foo',
+    'Bar',
+    'Foo',
+    'Baz',
+    'Qux',
+    'Baz',
+  ];
+
+  final uniqueNames = {...names2}; //This is the spread_operator
+  print(uniqueNames);
 
   // print('===================');
+  final ages1 = {20, 30, 40};
+  final ages2 = {20, 30, 40};
+
+  if (ages1 == ages2) {
+    print('Ages are equal');
+  } else {
+    print('Ages are not equal');
+
+  }
 
   // print('===================');
 
