0eed89c1200083cc4db878ce555ed12228bd1d46
Messi002 <rostandmessi2@gmail.com>
2023-01-14 11:57:35 +0100

    enumerations

--- DART_Full/enum_classes_mixins/bin/enum1.dart
+++ DART_Full/enum_classes_mixins/bin/enum1.dart
@@ -1,4 +1,23 @@
 //enhanced_enums are just enums with values...
+void main(List<String> args) {
+  final foo = Person(name: 'Foo', car: Car.teslaModelX);
+
+  switch (foo.car) {
+    case Car.teslaModelX:
+      print('Foo has a Tesla Model X = ${foo.car}');
+      break;
+    case Car.teslaModelY:
+      print('Foo has a Tesla Model Y = ${foo.car}');
+      break;
+  }
+}
+
+class Person {
+  final String name;
+  final Car car;
+
+  Person({required this.name, required this.car});
+}
 
 enum Car {
   teslaModelX(
@@ -7,7 +26,7 @@ enum Car {
     year: 2012,
   ),
 
-teslaModelY(
+  teslaModelY(
     manufacturer: 'Telsa',
     model: 'ModelY',
     year: 2012,
@@ -17,5 +36,6 @@ teslaModelY(
   final String model;
   final int year;
 
- const Car({required this.manufacturer, required this.model, required this.year});
+  const Car(
+      {required this.manufacturer, required this.model, required this.year});
 }
