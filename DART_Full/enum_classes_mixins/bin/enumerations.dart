f73386cbb29793b0c9c5100f06c3b26e9cc51341
Messi002 <rostandmessi2@gmail.com>
2023-01-14 11:29:39 +0100

    enumerations

--- DART_Full/enum_classes_mixins/bin/enumerations.dart
+++ DART_Full/enum_classes_mixins/bin/enumerations.dart
@@ -13,6 +13,20 @@ void main(List<String> args) {
   }
 
   //*****************Switching on enums */
+//It is advisable to use switch statements because when a new enum is added our switch
+//statement will break while our if statement won't and breaking is actually good because
+//it tells us something hasn't been handled yet.
+  switch (woof.type) {
+    case AnimalType.rabbit:
+      print('This is a rabbit');
+      break;
+    case AnimalType.cat:
+      print('This is a cat');
+      break;
+    case AnimalType.dog:
+      print('This is a dog');
+      break;
+  }
 }
 
 enum AnimalType {
