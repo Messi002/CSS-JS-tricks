f932847446aedae6bfe67a6cf0a9735ab4620077
Messi002 <rostandmessi2@gmail.com>
2023-01-15 21:14:27 +0100

    classes

--- DART_Full/enum_classes_mixins/bin/classes2.dart
+++ DART_Full/enum_classes_mixins/bin/classes2.dart
@@ -1,4 +1,13 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  final car = Car();
+
+  try {
+    car.drive(speed: 10);
+    car.drive(speed: -1);
+  } catch (e) {
+    print(e);
+  }
+}
 
 class Vehicle {
   final int wheelCount;
@@ -9,10 +18,10 @@ class Vehicle {
   String toString() => '$runtimeType with $wheelCount wheels';
 }
 
-class Car extends Vehicle {
+class Cars extends Vehicle {
   //if you don't create the super it is like you're creating a class without connecting it to its
   //parent at all, when it actually extends its parent
-  const Car() : super(4);
+  const Cars() : super(4);
   //const Car() : super(4); means whenever someone calls the car constructor with will always have 4 wheels
   //while with Car(super.wheelCount) means anybody creating the Car constructor should pass it a wheel
   //which will be sent to the Vehicle
@@ -26,7 +35,7 @@ class Bicycle extends Vehicle {
 //GETTERS AND SETTERS
 
 // class Person {
-  
+
 //   final String firstName;
 //   final String lastName;
 //   final String fullNames;
@@ -41,6 +50,39 @@ class Person {
   final String lastName;
   String get fullNames => '$firstName $lastName';
 
+//This is useful when some heavy calculation is needed only when the getter property
+//is called
   Person({required this.firstName, required this.lastName});
-  
+}
+
+//---------------------------------
+//METHODS IN CLASSES
+
+//----------------------
+//SETTERS
+class Car {
+  int _speed = 0;
+
+  int get speed => _speed;
+
+  set speed(int newSpeed) {
+    if (newSpeed < 0) {
+      throw Exception('Speed cannot be negative');
+    } else {
+      _speed = newSpeed;
+    }
+  }
+
+  void drive({required int speed}) {
+    //This is how we set setters
+    this.speed = speed;
+    print('Accelerating to $speed km/h');
+  }
+
+  void stop() {
+    speed = 0;
+
+    print('Stopping...');
+    print('Stopped!');
+  }
 }
