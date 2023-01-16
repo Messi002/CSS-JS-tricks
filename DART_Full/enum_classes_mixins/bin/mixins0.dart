f5efc35054bf137c87f015288fa3cda173e9ced0
Messi002 <rostandmessi2@gmail.com>
2023-01-16 11:22:00 +0100

    mixins0

--- DART_Full/enum_classes_mixins/bin/mixins0.dart
+++ DART_Full/enum_classes_mixins/bin/mixins0.dart
@@ -1,7 +1,6 @@
 //Since Dart doesn't have multiple inheritance, the solution to this is `Mixins`
 //Abstract fields on mixins means that any other mixin built on it must have that property
 
-
 void main(List<String> args) {
   final person = Person();
   person.jump(speed: 10);
@@ -31,4 +30,30 @@ class Person with HasSpeed, CanJump, CanWalk {
   double speed;
 
   Person() : speed = 0.0;
+}
+
+//Mixins are a datatype themselves, so below we are going to work on how to accept
+//a mixin in a function
+
+mixin HasFirstName {
+  String get firstName;
+}
+
+mixin HasLastName {
+  String get lastName;
+  
+}
+
+mixin HasFullName on HasFirstName, HasLastName {
+  String get fullName => '$firstName $lastName';
+}
+
+class PersonX with HasFirstName, HasLastName, HasFullName {
+  @override
+  final String firstName;
+
+  @override
+  final String lastName;
+
+  PersonX({required this.firstName, required this.lastName});
 }
