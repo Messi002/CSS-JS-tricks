83cfd5cb1bcaee0be8e7279a6ddfea1ff92d2a7f
Messi002 <rostandmessi2@gmail.com>
2023-01-16 11:05:14 +0100

    mixins0

--- DART_Full/enum_classes_mixins/bin/mixins0.dart
+++ DART_Full/enum_classes_mixins/bin/mixins0.dart
@@ -1,23 +1,34 @@
 //Since Dart doesn't have multiple inheritance, the solution to this is `Mixins`
 //Abstract fields on mixins means that any other mixin built on it must have that property
 
-void main(List<String> args) {}
+
+void main(List<String> args) {
+  final person = Person();
+  person.jump(speed: 10);
+  person.walk(speed: 6);
+}
 
 mixin HasSpeed {
   abstract double speed;
 }
 
-mixin CanJump on HasSpeed{
+mixin CanJump on HasSpeed {
   void jump({required double speed}) {
     print('$runtimeType is jumping at the speed of $speed');
     this.speed = speed;
   }
 }
 
-
-mixin CanWalk on CanJump{
-  void jump({required double speed}) {
-    print('$runtimeType is jumping at the speed of $speed');
+mixin CanWalk on HasSpeed {
+  void walk({required double speed}) {
+    print('$runtimeType is walking at the speed of $speed');
     this.speed = speed;
   }
+}
+
+class Person with HasSpeed, CanJump, CanWalk {
+  @override
+  double speed;
+
+  Person() : speed = 0.0;
 }
