2056a36140b3da250b181274237d41035499e98f
Messi002 <rostandmessi2@gmail.com>
2023-01-17 13:06:23 +0100

    custom operators

--- DART_Full/customOper_extensions_generics/bin/custom_oper1.dart
+++ DART_Full/customOper_extensions_generics/bin/custom_oper1.dart
@@ -1,11 +1,22 @@
 //Custom operators are like functions in which you add functionality the way you want
+//Sync generators return an Iterable
+//Async generators return a Stream
 
-void main(List<String> args) {}
+void main(List<String> args) {
+  final dad = FamilyMember(name: 'dad');
+  final mom = FamilyMember(name: 'mom');
+
+  final fam = dad + mom;
+  print(fam);
+  print('----------------');
+  const names = ['Seth', 'Kathy', 'Ethan', 'Megan'];
+  print(names * 2);
+}
 
 class FamilyMember {
   final String name;
 
-  FamilyMember({required this.name});
+  const FamilyMember({required this.name});
 
   @override
   String toString() => 'Family member (name = $name)';
@@ -14,8 +25,21 @@ class FamilyMember {
 class Family {
   final List<FamilyMember> members;
 
-  Family({required this.members});
+  const Family({required this.members});
 
   @override
   String toString() => 'Family (members = $members)';
+}
+
+extension ToFamily on FamilyMember {
+  Family operator +(FamilyMember other) => Family(members: [this, other]);
+}
+
+//-------------------------------------
+extension Times<T> on Iterable<T> {
+  Iterable<T> operator *(int times) sync* {
+    for (var i = 0; i < times; i++) {
+      yield* this;
+    }
+  }
 }
