d9f62ab36623c329b3296fbdf21ac1c140309d1f
Messi002 <rostandmessi2@gmail.com>
2023-01-18 16:50:15 +0100

    extensions

--- DART_Full/customOper_extensions_generics/bin/extensions2.dart
+++ DART_Full/customOper_extensions_generics/bin/extensions2.dart
@@ -4,6 +4,9 @@ void main(List<String> args) {
   print(add.callWithRandomValues());
   print(subtract.callWithRandomValues());
   print('-----------------------');
+  const jack = Person(name: 'Jack Dowe', age: 28);
+  print(ShortDescription(jack).description);
+  print('-----------------------');
 }
 
 //Extensions on typedef
@@ -20,6 +23,31 @@ extension on IntFunction {
     final rnd1 = Random().nextInt(100);
     final rnd2 = Random().nextInt(100);
 //Call is something Dart uses to allow you call a function without its name
-    return call(rnd1, rnd2);
+    //return this.call(rnd1, rnd2); or this other method....
+    return call(
+      rnd1,
+      rnd2,
+    );
   }
+}
+
+//Why some extensions have name others don't?
+//When two extensions haven't been given a name and we don't know 
+//how to call it or go about it
+
+class Person {
+  final String name;
+  final int age;
+
+  const Person({required this.name, required this.age});
+}
+
+//Short description
+extension ShortDescription on Person {
+  String get description => '$name ($age)';
+}
+
+//long description
+extension LongDescription on Person {
+  String get description => '$name is $age old';
 }
