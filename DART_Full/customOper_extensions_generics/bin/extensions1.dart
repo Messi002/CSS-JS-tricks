29e1ad864b4bc92010f3796c7cef6ff4bcef2a9a
Messi002 <rostandmessi2@gmail.com>
2023-01-18 15:09:10 +0100

    custom_operators

--- DART_Full/customOper_extensions_generics/bin/extensions1.dart
+++ DART_Full/customOper_extensions_generics/bin/extensions1.dart
@@ -3,6 +3,12 @@ void main(List<String> args) {
   print("----------------------");
   String value = 'Hello'.reversed;
   print(value);
+  print("----------------------");
+  final value1 = [1, 2, 3].sum;
+  final value2 = [1.1, 2.2, 3.4].sum;
+  print(value1);
+  print(value2);
+  print("----------------------");
 }
 
 //Introduction...
@@ -13,4 +19,10 @@ extension on int {
 //To reverse a string
 extension on String {
   String get reversed => split('').reversed.join('');
+}
+
+//For summing an iterable
+extension SumOfIterables<T extends num> on Iterable<T> {
+  T get sum => fold(0 as T, (a, b) => a + b as T);
+  // T get sum => reduce((a, b) => a + b as T);
 }
