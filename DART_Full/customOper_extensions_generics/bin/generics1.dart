44e871415736c90fca95939b229b55fe650b3de2
Messi002 <rostandmessi2@gmail.com>
2023-01-19 02:40:55 +0100

    generics

--- DART_Full/customOper_extensions_generics/bin/generics1.dart
+++ DART_Full/customOper_extensions_generics/bin/generics1.dart
@@ -2,11 +2,17 @@
 //T means some sort of generic type
 
 void main(List<String> args) {
+  //Here the call site defines the type to be returned from the function
   final double doubleValue = eitherIntOrDouble();
   final int intValue = eitherIntOrDouble();
   print(doubleValue);
   print(intValue);
   // final String stringValue = eitherIntOrDouble();
+  print('--------------------');
+  print(doVariablesMatch(1, 2));
+  print(doVariablesMatch(1, 2.2));
+  print(doVariablesMatch(1, 'Foo'));
+  print(doVariablesMatch('Foo', 'Bar'));
   print('--------------------');
 }
 
@@ -20,3 +26,11 @@ T eitherIntOrDouble<T extends num>() {
       throw Exception('Not Supported');
   }
 }
+
+//Checking the types of two variables
+
+//method1
+// bool doVariablesMatch(Object a, Object b) => a.runtimeType == b.runtimeType;
+
+//method2
+bool doVariablesMatch<L, R>(L a, R b) => L == R;
