bd49e165b70307bff9ee1fee16c38edfce5aabaf
Messi002 <rostandmessi2@gmail.com>
2023-01-22 22:04:55 +0100

    errors

--- DART_Full/error_async_isolate/bin/error5.dart
+++ DART_Full/error_async_isolate/bin/error5.dart
@@ -1,4 +1,18 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  final person = Person(age: 10);
+  print(person);
+  person.age = 0;
+  print(person);
+
+  try {
+    person.age = -1;
+    print(person);
+  } catch (e) {
+    print(e);
+  } finally {
+    print('finally blocked reached');
+  }
+}
 
 class Person {
   int _age;
@@ -11,5 +25,31 @@ class Person {
       throw ArgumentError('Age cannot be negative');
     }
     _age = value;
+  }
+
+  @override
+  String toString() => 'Person( age: $age)';
+}
+
+//Capturing stacktrace for exception
+class DogIsTiredException implements Exception {
+  final String message;
+
+  DogIsTiredException([this.message = 'Poor doggy is tired']);
+}
+
+
+class Dog {
+  final bool isTired;
+
+  Dog({required this.isTired});
+
+  @override
+  void run() {
+    if (isTired) {
+      throw DogIsTiredException();
+    }else {
+      print('Dog is running...');
+    }
   }
 }
