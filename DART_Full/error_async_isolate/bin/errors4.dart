e1bb557707b58511e35585f5b8cb8f0b9197a22d
Messi002 <rostandmessi2@gmail.com>
2023-01-22 18:44:28 +0100

    errors

--- DART_Full/error_async_isolate/bin/errors4.dart
+++ DART_Full/error_async_isolate/bin/errors4.dart
@@ -1,4 +1,18 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  try {
+    print('Dog aged 11 is going to run...');
+    Dog(isTired: false, age: 11).run();
+  } catch (e) {
+    print(e);
+  }
+  print('----------------');
+    try {
+    print('A tired dog aged 2 is going to run...');
+    Dog(isTired: true, age: 2).run();
+  } catch (e) {
+    print(e);
+  }
+}
 
 //You can in dart annotate a function with a class
 //a specific function throws a specific error, we are going to annotate them
@@ -6,7 +20,7 @@ void main(List<String> args) {}
 class Throws<T> {
   final List<T> exceptions;
 
- const Throws(this.exceptions);
+  const Throws(this.exceptions);
 }
 
 class DogIsTooOldException implements Exception {
@@ -20,9 +34,27 @@ class DogIsTiredException implements Exception {
 class Animal {
   final int age;
 
-  Animal({required this.age});
+  const Animal({required this.age});
 
 //This is how annotation of errors is done...
- @Throws([UnimplementedError])
+  @Throws([UnimplementedError])
   void run() => throw UnimplementedError();
+}
+
+class Dog extends Animal {
+  final bool isTired;
+
+  Dog({required this.isTired, required super.age});
+
+  @Throws([DogIsTiredException(), DogIsTooOldException()])
+  @override
+  void run() {
+    if (age > 10) {
+      throw const DogIsTooOldException();
+    } else if (isTired) {
+      throw const DogIsTiredException();
+    } else {
+      print('Dog is running...');
+    }
+  }
 }
