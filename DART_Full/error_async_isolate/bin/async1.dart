3946763fc5b112e21322bd2ef847eba406a714d4
Messi002 <rostandmessi2@gmail.com>
2023-01-23 10:58:44 +0100

    asynchronous

--- DART_Full/error_async_isolate/bin/async1.dart
+++ DART_Full/error_async_isolate/bin/async1.dart
@@ -2,9 +2,17 @@
 //For a normal function the execution will be stopped until the result is gotten
 //For a Future function, the execution continues since and gets back to where the result is wanted only later
 
-void main(List<String> args) async {}
+void main(List<String> args) async {
+  print(await getName());
+  print(await getAddress());
+  print(await getCity());
+}
 
 Future<String> getName() async => 'John Doe';
-Future<String> getAddress()  => Future.value('123 Main St.');
-Future<String> getPhoneNumber()  => Future.delayed(const Duration(seconds: 1), () => '555-555-555');
-
+Future<String> getAddress() => Future.value('123 Main St.');
+Future<String> getPhoneNumber() =>
+    Future.delayed(const Duration(seconds: 1), () => '555-555-555');
+Future<String> getCity() async {
+  await Future.delayed(const Duration(seconds: 2));
+  return 'New York';
+}
