6c7d68f1206c686e0f4b5ccc96f6acfcfa736565
Messi002 <rostandmessi2@gmail.com>
2023-01-24 21:17:29 +0100

    asynchronous

--- DART_Full/error_async_isolate/bin/async3.dart
+++ DART_Full/error_async_isolate/bin/async3.dart
@@ -3,16 +3,25 @@
 import 'dart:async';
 
 void main(List<String> args) async {
-  final controller = StreamController<String>();
+  // final controller = StreamController<String>();
 
-  controller.sink.add('Hello');
-  controller.sink.add('world');
+  // controller.sink.add('Hello');
+  // controller.sink.add('world');
 
-  await for (var value in controller.stream) {
-    print(value);
+  // await for (var value in controller.stream) {
+  //   print(value);
+  // }
+
+  // controller.close();
+  print('--------------------');
+  await for (var name1 in names.capitalized) {
+    print(name1);
   }
-
-  controller.close();
+  print('--------------------');
+   await for (var name2 in names.capitalizedUsingMap) {
+    print(name2);
+  }
+  print('--------------------');
 }
 
 //Stream transformer...
@@ -21,3 +30,16 @@ Stream<String> names = Stream.fromIterable([
   'Jake',
   'Woods',
 ]);
+
+extension on Stream<String> {
+  Stream<String> get capitalized => transform(ToUpperCase());
+
+  Stream<String> get capitalizedUsingMap => map((name) => name.toUpperCase());
+}
+
+class ToUpperCase extends StreamTransformerBase<String, String> {
+  @override
+  Stream<String> bind(Stream<String> stream) {
+    return stream.map((name) => name.toUpperCase());
+  }
+}
