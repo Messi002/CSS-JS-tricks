623f0c0eb6a4f71cdb940dfc29a06cccbd50290b
Messi002 <rostandmessi2@gmail.com>
2023-01-25 20:14:00 +0100

    asynchronous

--- DART_Full/error_async_isolate/bin/async3.dart
+++ DART_Full/error_async_isolate/bin/async3.dart
@@ -13,16 +13,6 @@ void main(List<String> args) async {
   // }
 
   // controller.close();
-  print('--------------------');
-  await for (var name1 in names.capitalized) {
-    print(name1);
-  }
-  print('--------------------');
-   await for (var name2 in names.capitalizedUsingMap) {
-    print(name2);
-  }
-  print('--------------------');
-}
 
 //Stream transformer...
 Stream<String> names = Stream.fromIterable([
