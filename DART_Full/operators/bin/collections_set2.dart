48245f717a938f45ef0b5798665accd44feff879
Messi002 <rostandmessi2@gmail.com>
2023-01-27 15:22:09 +0100

    committing today

--- DART_Full/operators/bin/collections_set2.dart
+++ DART_Full/operators/bin/collections_set2.dart
@@ -40,20 +40,4 @@ class Dog {
   final String name;
   final int age;
 
-  Dog({required this.name, required this.age});
 
-  @override
-  String toString() => 'Dog: $name, $age';
-
-//Just because the hash value of an object is the same
-//it doesn't necessarily mean it is considered to be equal
-  @override
-  int get hashCode => Object.hash(name, age);
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Dog && name == other.name && age == other.age;
-
-
-}
