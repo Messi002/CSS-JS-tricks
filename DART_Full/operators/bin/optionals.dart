166beb3a38d14d90513dfc6fc1a9c4462ff77ece
Messi002 <rostandmessi2@gmail.com>
2023-01-28 18:03:53 +0100

    committing today

--- DART_Full/operators/bin/optionals.dart
+++ DART_Full/operators/bin/optionals.dart
@@ -63,4 +63,18 @@ extension DefaultValues<T> on T? {
     final shadow = this;
     if (shadow != null) {
       return shadow;
-
+    }
+    switch (T) {
+      case String:
+        return 'X' as T;
+      case int:
+        return 0 as T;
+      case double:
+        return 0.0 as T;
+      case bool:
+        return false as T;
+      default:
+        throw Exception('No default value for type $T');
+    }
+  }
+}
