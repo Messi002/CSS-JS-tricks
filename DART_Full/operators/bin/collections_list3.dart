77cec9efb2ab32d40d2f25c47ffc6aa3b048a643
Messi002 <rostandmessi2@gmail.com>
2023-01-29 13:47:29 +0100

    committing today

--- DART_Full/operators/bin/collections_list3.dart
+++ DART_Full/operators/bin/collections_list3.dart
@@ -1,23 +1,4 @@
-// void main(List<String> args) {
-//   for (var name in getNames()) {
-//     print(name);
-//   }
-// }
 
-
-//************FOR_SNYCHRONOUS_CODE */
-// Iterable<String> getNames() sync* {
-//   print('producing Bob');
-//   yield 'Bob';
-//   print('producing Alice');
-//   yield 'Alice';
-//   print('producing John');
-//   yield 'John';
-//   print('producing template_names');
-//   //the yield with an asterik(*) is used for injecting another
-//   //return value into already a synchronous code
-//   yield* templateNames();
-// }
 
 // Iterable<String> templateNames() sync* {
 //   print('producing Foo');
@@ -40,7 +21,14 @@ void addToArrayWrong() {
   final names2 = ['Foo2', 'Bar2', 'Baz2'];
 
 //This is wrong because you aren't copying the content of names1 but rather
+//assignin it to allNamesWrong reason why it is altered
+  final allNamesWrong = names1;
+  allNamesWrong.addAll(names2);
 
+  print(names1);
+  print(names2);
+  print(allNamesWrong);
+}
 
 void addToArrayRight() {
   final names1 = ['Foo1', 'Bar1', 'Baz1'];
