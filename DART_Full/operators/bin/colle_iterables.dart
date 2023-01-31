2f3293aafe269e6d02fc3d88169f355cd4c016e6
Messi002 <rostandmessi2@gmail.com>
2023-01-31 18:25:02 +0100

    committing today

--- DART_Full/operators/bin/colle_iterables.dart
+++ DART_Full/operators/bin/colle_iterables.dart
@@ -2,18 +2,5 @@
 
 
 
-void main(List<String> args) {
-  const names = ['John', 'Jane', 'Jack', 'Jilli'];
-  final upperCase = names.map((name) {
-    print('map got called');
-    return name.toUpperCase();
-  });
-  for (var name in upperCase.take(3)) {
-    print(name);
-  }
-  // print('===================');
 
-  // print('===================');
-
-  // print('===================');
 }
