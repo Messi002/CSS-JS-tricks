be122dcbfa7a04ec38fc8ce7649258854a704daa
Messi002 <rostandmessi2@gmail.com>
2023-02-02 20:53:11 +0100

    committing today

--- DART_Full/enum_classes_mixins/bin/enum1.dart
+++ DART_Full/enum_classes_mixins/bin/enum1.dart
@@ -1,5 +1,16 @@
 //enhanced_enums are just enums with values...
+// void main(List<String> args) {
+//   final foo = Person(name: 'Foo', car: Car.teslaModelX);
 
+//   switch (foo.car) {
+//     case Car.teslaModelX:
+//       print('Foo has a Tesla Model X = ${foo.car}');
+//       break;
+//     case Car.teslaModelY:
+//       print('Foo has a Tesla Model Y = ${foo.car}');
+//       break;
+//   }
+// }
 
 // class Person {
 //   final String name;
