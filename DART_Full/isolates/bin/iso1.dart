352a807bd38e5abedf0cb62f79fa4b70e141d200
Messi002 <rostandmessi2@gmail.com>
2023-02-04 01:05:16 +0100

    dart_application_using_isolates

---
+++ DART_Full/isolates/bin/iso1.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) async {}
+
+const messagesAndResponse = {
+  '' : 'Ask me a question like "How are you?"',
+  'Hello' : 'Hi',
+  'How are you doing?' : 'Fine!',
+  'What are you doing?' : 'Learning about isolates in Dart!',
+  'Are you having fun?' : 'Yeah sure!',
+};
